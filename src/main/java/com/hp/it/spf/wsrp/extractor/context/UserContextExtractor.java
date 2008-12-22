c50b3990be7a712fa19055754591a95ef8604284
liuliyey <liuliyey@mycompany.com>
2008-12-22 08:49:23 +0000

    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/wsrp/trunk/soap-extractor@137 fbe1f26e-0b55-418f-9102-102d79b04d0f

---
+++ src/main/java/com/hp/it/spf/wsrp/extractor/context/UserContextExtractor.java
@@ -0,0 +1,254 @@
+package com.hp.it.spf.wsrp.extractor.context;
+
+import org.apache.log4j.Logger;
+
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.Name;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.Node;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Set;
+
+import com.hp.it.spf.wsrp.extractor.profile.ProfileHelper;
+
+/**
+ * Extracts user context data from the appropriate SOAP header elements and make them available to portlets
+ * as map in portlet request.
+ */
+public class UserContextExtractor implements SOAPHandler{
+
+	public static final String USER_CONTEXT_KEYS_KEY = "com.hp.spp.UserContextKeys";
+	public static final String USER_PROFILE_KEY = "com.hp.spp.UserProfile";
+	
+	public static final String[] SOAP_ACTION = {"\"urn:oasis:names:tc:wsrp:v1:getMarkup\"",
+												"\"urn:oasis:names:tc:wsrp:v2:getMarkup\"",
+												"\"urn:oasis:names:tc:wsrp:v1:performBlockingInteraction\"",
+												"\"urn:oasis:names:tc:wsrp:v2:performBlockingInteraction\"",
+												"\"urn:oasis:names:tc:wsrp:v2:handleEvents\"",
+												"\"urn:oasis:names:tc:wsrp:v2:getResource\""
+	                                            };
+
+	private static final Logger mLog = Logger.getLogger(UserContextExtractor.class);
+	private static final Logger mTLog = Logger.getLogger("TIME." + UserContextExtractor.class.getName());
+	public static final ProfileHelper PROFILE_HELPER = new ProfileHelper();
+	
+	public Set getHeaders() {
+        return null;
+    }
+	
+	public boolean handleMessage(MessageContext mc) {
+		if(mc instanceof SOAPMessageContext){
+			if(isRequest(mc) && isNeedHandledSOAPAction(mc)){
+				invoke((SOAPMessageContext)mc);
+			}
+		}
+		return true;
+	}
+
+	public boolean handleFault(MessageContext mc) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public void close(MessageContext mc) {
+		// TODO Auto-generated method stub
+		
+	}    
+    
+	/**
+	 * Extracts the context data and make it available to portlets.
+	 * @param messageContext current message context
+	 * @throws AxisFault If unexpected error occurs during the processing.
+	 */
+	public void invoke(SOAPMessageContext messageContext) {		
+		
+		long start = 0;
+		if (mTLog.isDebugEnabled()) {
+			start = System.currentTimeMillis();
+		}
+		Map userContextKeys = null;
+		Map userProfile = null;
+		try {
+			Map userContext = extractUserContext(messageContext.getMessage().getSOAPPart().getEnvelope());
+			if (userContext != null) {
+				userContextKeys = (Map) userContext.get("com.hp.spp.UserContextKeys");
+				userProfile = (Map) userContext.get("com.hp.spp.UserProfile");
+			}
+			else {
+				// SPP continues to send data in SPP 2.0 compatibility mode
+				userContextKeys = extractUserContextKeys(messageContext.getMessage().getSOAPPart().getEnvelope());
+				userProfile = extractUserProfile(messageContext.getMessage().getSOAPPart().getEnvelope());
+			}
+			if (mLog.isDebugEnabled()) {
+				mLog.debug("User context keys: " + userContextKeys);
+				mLog.debug("User profile: " + userProfile);
+			}
+			
+			makeContextDataAvailableToPortlets(messageContext, userContextKeys, userProfile);
+		}
+		catch (SOAPException e) {
+			mLog.error("Error while retrieving context keys", e);			
+		}
+		if (mTLog.isDebugEnabled()) {
+			mTLog.debug("context keys extracted | " + (System.currentTimeMillis()-start));
+		}
+	}
+
+	private Map extractUserContext(SOAPEnvelope envelope) throws SOAPException {
+		Name headerName = envelope.createName("UserContext", "spp", "http://www.hp.com/spp");
+		SOAPHeader header = envelope.getHeader();
+		Iterator it = header.getChildElements(headerName);
+		if (it.hasNext()) {
+			SOAPElement headerElement = (SOAPElement) it.next();
+			if (headerElement.hasChildNodes()) {
+				String userContextString = headerElement.getFirstChild().getNodeValue();
+				try {
+					return PROFILE_HELPER.profileFromString(userContextString);
+				}
+				catch (IllegalArgumentException e) {
+					mLog.error("Error parsing context", e);
+				}
+			}
+		}
+		// if you get here this means that we didn't find the node
+		if (mLog.isDebugEnabled()) {
+			mLog.debug("UserContext node not found in the header");
+		}
+		return null;
+	}
+
+	/**
+	 * Extracts user context keys from SOAP header and returns them as map. The map keys are the
+	 * same as the element names within <code>UserContextKeys</code> tag.
+	 * @param envelope parent envelope for the SOAP header
+	 * @return map containing user context key (String) names and their values
+	 * @throws SOAPException If an error occurs during envelope processing
+	 */
+	private Map extractUserContextKeys(SOAPEnvelope envelope) throws SOAPException {
+		Name headerName = envelope.createName("UserContextKeys", "spp", "http://www.hp.com/spp");
+		SOAPHeader header = envelope.getHeader();
+		Iterator it = header.getChildElements(headerName);
+		if (it != null && it.hasNext()) {
+			SOAPElement headerElement = (SOAPElement) it.next();
+			Map userContextKeys = new HashMap();
+			for (Iterator it2 = headerElement.getChildElements(); it2.hasNext();) {
+				SOAPElement keyEl = (SOAPElement) it2.next();
+				Iterator it3 = keyEl.getChildElements();
+				if (it3.hasNext()) {
+					userContextKeys.put(keyEl.getElementName().getLocalName(), ((Node) it3.next()).getValue());
+				}
+				else {
+					String key = keyEl.getElementName().getLocalName();
+					userContextKeys.put(key, "");
+				}
+			}
+			return userContextKeys;
+		}
+		else {
+			mLog.error("No elements in the header");
+		}
+		return null;
+	}
+
+	/**
+	 * Extracts user profile from the SOAP header and returns it as a Map. The profile is encoded
+	 * as a string in <tt>UserProfile</tt> tag.
+	 * @param envelope parent envelope of the SOAP header
+	 * @return profile map
+	 * @throws SOAPException If an error occurs when manipulating SOAP elements.
+	 * @see ProfileHelperff#profileFromString(String) for details about how the profile is encoded.
+	 */
+	private Map extractUserProfile(SOAPEnvelope envelope) throws SOAPException {
+		Name headerName = envelope.createName("UserProfile", "spp", "http://www.hp.com/spp");
+		SOAPHeader header = envelope.getHeader();
+		Iterator it = header.getChildElements(headerName);
+		if (it.hasNext()) {
+			SOAPElement headerElement = (SOAPElement) it.next();
+			if (headerElement.hasChildNodes()) {
+				String userProfileString = headerElement.getFirstChild().getNodeValue();
+				try {
+					return PROFILE_HELPER.profileFromString(userProfileString);
+				}
+				catch (IllegalArgumentException e) {
+					mLog.error("Error parsing profile", e);
+				}
+			}
+		}
+
+		// if you get here this means that we didn't find the node
+		if (mLog.isDebugEnabled()) {
+			mLog.debug("UserProfile node not found in the header");
+		}
+		return null;
+	}
+
+	/**
+	 * Puts the context data (keys and profile) into {@link HttpServletRequest} of the WSRP call making it available
+	 * to portlets through the portlet request.
+	 * @param messageContext message context used to access the request
+	 * @param userContextKeys context keys map to bind
+	 * @param userProfile user profile map to bind
+	 */
+	private void makeContextDataAvailableToPortlets(SOAPMessageContext messageContext, Map userContextKeys, Map userProfile) {
+		HttpServletRequest request =
+				(HttpServletRequest) messageContext.get(MessageContext.SERVLET_REQUEST);
+		if (userContextKeys != null) {
+			if (mLog.isDebugEnabled()) {
+				mLog.debug("UserContextKeys bound to request attribute " + USER_CONTEXT_KEYS_KEY);
+			}
+			request.setAttribute(USER_CONTEXT_KEYS_KEY, userContextKeys);
+		}
+		else {
+			mLog.error("UserContextKeys not found");
+		}
+		if (userProfile != null) {
+			if (mLog.isDebugEnabled()) {
+				mLog.debug("UserProfile bound to request attribute " + USER_PROFILE_KEY);
+			}
+			request.setAttribute(USER_PROFILE_KEY, userProfile);
+		}
+	}
+	
+	/**
+	 * Check request operation
+	 * @param mc
+	 * @return is request
+	 */
+	private boolean isRequest(MessageContext mc){
+		return !((Boolean)mc.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY)).booleanValue();
+	}
+	
+	/**
+	 * Check currect soap action is need to be handled
+	 * @param mc
+	 * @return true/false
+	 */
+	private boolean isNeedHandledSOAPAction(MessageContext mc){
+		try{
+			// SOAP action list which should be handled
+			List list = Arrays.asList(SOAP_ACTION);
+			
+			//retrieve the current soap action
+			String currentActionURI = (String)mc.get("javax.xml.ws.soap.http.soapaction.uri");
+			
+			return list.contains(currentActionURI);
+			
+		}catch(Exception ex){
+			mLog.debug("Retrieve soap action error:" + ex.getMessage());			
+		}
+		return false;
+	}
+}
---
+++ src/main/java/com/hp/it/spf/wsrp/extractor/profile/ProfileHelper.java
@@ -0,0 +1,381 @@
+package com.hp.it.spf.wsrp.extractor.profile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+
+/**
+ * Class with several helper methods working on the SPP profile representation.
+ *
+ * This class contains methods that can serialize and deserialize the profile
+ * map into and from String object. The grammar is as follows: <code>
+ * Profile: Map
+ * Map: "{" (AttributeName "=" AttributeValue ", ")* "}"
+ * AttributeName: String
+ * AttributeValue: String | Map | List
+ * List: "[" (AttributeValue ", ")* "]
+ * </code>
+ */
+public class ProfileHelper {
+
+	Logger mLog = Logger.getLogger(ProfileHelper.class);
+
+
+	/**
+	 * Returns a map that contains only the profile entries for which the values
+	 * are represented as strings. The entries that are Maps or Lists are not
+	 * copied from <tt>profile</tt> map.
+	 *
+	 * @param profile
+	 *            profile map to copy data from
+	 * @return map containing only entries with String values.
+	 */
+	public Map toLegacyProfile(Map profile) {
+		if (profile == null) {
+			return null;
+		}
+		Map result = new HashMap();
+		for (Iterator it = profile.entrySet().iterator(); it.hasNext();) {
+			Map.Entry entry = (Map.Entry) it.next();
+			if (entry.getValue() instanceof String) {
+				result.put(entry.getKey(), entry.getValue());
+			}
+		}
+		return result;
+	}
+
+
+		/**
+		* Transforms simulated user profile into an legacy profile. The simulated user
+		* information key's of Constants.MAP_SIMULATOR.key
+		* @param java.util.HashMap
+		* @return java.util.Map containing information about both LoggedIn user and may contain Simulated User
+		*	information in case if logged in user is in simulation mode (and also depends on the modes)
+		*
+		*/
+		public Map toLegacySimulatingProfile(Map map){
+				Map legacyProfile = toLegacyProfile(map);
+
+				//if the user is simulated user then get the simulated map
+				 String isSimulating = (String) map.get(Constants.MAP_IS_SIMULATING);
+				 if (isSimulating!=null && isSimulating.equalsIgnoreCase("true")){
+					 Map simulatedMap = (java.util.Map)map.get(Constants.MAP_SIMULATOR);
+					 Iterator it = simulatedMap.entrySet().iterator();
+					 while (it.hasNext()) {
+					     Map.Entry pairs = (Map.Entry)it.next();
+					     Object value = pairs.getValue();
+					     if(value instanceof String)
+					    	 legacyProfile.put(Constants.MAP_SIMULATOR + "." + (String) pairs.getKey(), value);
+					 }
+
+				 }
+
+			return legacyProfile;
+	}
+
+	/**
+	 * Returns a map that contains only the profile entries for which the values
+	 * are represented as strings. The entries that are Maps or Lists are not
+	 * copied from <tt>profile</tt> map.
+	 *
+	 * @param profile
+	 *            profile map to copy data from
+	 * @return map containing only entries with String values.
+	 * @throws ParserConfigurationException
+	 */
+	public Document profileToLegacyXml(Map profile) throws ParserConfigurationException {
+		if (profile == null) {
+			return null;
+		}
+		Map legacyProfile = toLegacyProfile(profile);
+		Document document = null;
+
+		// New Document creation
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder constructor = factory.newDocumentBuilder();
+
+		document = constructor.newDocument();
+
+		Element root = document.createElement("user");
+		Iterator it = legacyProfile.keySet().iterator();
+
+		while (it.hasNext()) {
+			String paramName = (String) it.next();
+			Element paramNode = document.createElement("attribute");
+			paramNode.setAttribute("name", paramName);
+			String paramMapValue = (String) legacyProfile.get(paramName);
+			if (paramMapValue.indexOf(";") != 0) {
+				String[] insideParamMap = paramMapValue.split(";");
+
+				for (int i = 0; i < insideParamMap.length; i++) {
+					if (!insideParamMap[i].equals(null)
+							&& !insideParamMap[i].trim().equalsIgnoreCase("")) {
+						Element paramNodeValue = document.createElement("value");
+						Node paramValue = document.createTextNode(insideParamMap[i]);
+						paramNodeValue.appendChild(paramValue);
+						paramNode.appendChild(paramNodeValue);
+					}
+				}
+
+			} else {
+				Element paramNodeValue = document.createElement("value");
+				Node paramValue = document.createTextNode(paramMapValue);
+				paramNodeValue.appendChild(paramValue);
+				paramNode.appendChild(paramNodeValue);
+			}
+
+			root.appendChild(paramNode);
+		}
+		document.appendChild(root);
+		return document;
+	}
+
+
+	/**
+	 * Serializes the profile map to String using "{" and "}" separators for
+	 * Maps and "[" and "]" separators for lists.
+	 *
+	 * @param profile
+	 *            profile map
+	 * @return profile map String representation
+	 */
+	public String profileToString(Map profile) {
+		StringBuffer sb = new StringBuffer();
+		writeMap(sb, profile);
+		return sb.toString();
+	}
+
+	/**
+	 * Reads profile map from a String representation using "{" and "}"
+	 * separators for Maps and "[" and "]" separators for lists. The separator
+	 * characters are properly un-escaped.
+	 *
+	 * @param s
+	 *            string represnetation of the profile
+	 * @return deserialized from string profile map
+	 * @throws IllegalArgumentException
+	 *             If the string <tt>s</tt> has incorrect format
+	 */
+	public Map profileFromString(String s) {
+		Stack stack = new Stack();
+		readMap(stack, s, 0);
+		return (Map) stack.pop();
+	}
+
+	protected void writeObject(StringBuffer sb, Object obj) {
+		if (obj == null) {
+			sb.append("");
+		} else if (obj instanceof Map) {
+			writeMap(sb, (Map) obj);
+		} else if (obj instanceof List) {
+			writeList(sb, (List) obj);
+		} else {
+			escape(sb, String.valueOf(obj));
+		}
+	}
+
+	protected void writeMap(StringBuffer sb, Map map) {
+		sb.append('{');
+		if (map != null && !map.isEmpty()) {
+			for (Iterator it = map.entrySet().iterator(); it.hasNext();) {
+				Map.Entry entry = (Map.Entry) it.next();
+				sb.append(entry.getKey()).append('=');
+				writeObject(sb, entry.getValue());
+				if (it.hasNext()) {
+					sb.append(", ");
+				}
+			}
+		}
+		sb.append('}');
+	}
+
+	protected void writeList(StringBuffer sb, List lst) {
+		sb.append('[');
+		if (lst != null && !lst.isEmpty()) {
+			for (Iterator it = lst.iterator(); it.hasNext();) {
+				writeObject(sb, it.next());
+				if (it.hasNext()) {
+					sb.append(", ");
+				}
+			}
+		}
+		sb.append(']');
+	}
+
+	protected void escape(StringBuffer sb, String s) {
+		if (s == null || s.length() == 0) {
+			return;
+		}
+		for (int i = 0, len = s.length(); i < len; ++i) {
+			char c = s.charAt(i);
+			if (c == '[' || c == ']' || c == '{' || c == '}' || c == ','
+					|| c == '=') {
+				sb.append('\\').append(c);
+			} else {
+				sb.append(c);
+			}
+		}
+	}
+
+	protected int readObject(Stack stack, String s, int pos) {
+		if (s == null) {
+			throw new IllegalArgumentException("String cannot be null");
+		}
+		if (s.length() == 0) {
+			stack.push("");
+			return pos;
+		} else {
+			char c = s.charAt(pos);
+			if (c == '[') {
+				return readList(stack, s, pos);
+			} else if (c == '{') {
+				return readMap(stack, s, pos);
+			} else if (!stack.isEmpty()) {
+				if (stack.peek() instanceof List) {
+					// look for end of element or end of the list
+					int posEnd = pos;
+					while (posEnd < s.length() && !eqAt(s, posEnd, ',')
+							&& !eqAt(s, posEnd, ']')) {
+						posEnd++;
+					}
+					if (posEnd == s.length()) {
+						throw new IllegalArgumentException(
+								"Error parsing list - missing closing ']'");
+					}
+					stack.push(unescape(s, pos, posEnd));
+					return posEnd;
+				} else if (stack.peek() instanceof Map) {
+					// look for end of entry or end of the map
+					int posEnd = pos;
+					while (posEnd < s.length() && !eqAt(s, posEnd, ',')
+							&& !eqAt(s, posEnd, '}')) {
+						posEnd++;
+					}
+					if (posEnd == s.length()) {
+						throw new IllegalArgumentException(
+								"Error parsing map - missing closing '}'");
+					}
+					stack.push(unescape(s, pos, posEnd));
+					return posEnd;
+				}
+			} else {
+				stack.push(unescape(s, pos, s.length()));
+				return s.length();
+			}
+		}
+		throw new IllegalStateException("How do we get here?");
+	}
+
+	protected int readMap(Stack stack, String s, int pos) {
+		if (s.charAt(pos) != '{') {
+			throw new IllegalArgumentException("Error parsing map: "
+					+ s.substring(pos));
+		}
+		Map map = new HashMap();
+		stack.push(map);
+		// skip '{'
+		pos += 1;
+		while (pos < s.length() && !eqAt(s, pos, '}')) {
+			pos = readKey(stack, s, pos);
+			String name = (String) stack.pop();
+			// skip '='
+			pos += 1;
+			pos = readObject(stack, s, pos);
+			Object value = stack.pop();
+			map.put(name, value);
+			// skip ", "
+			if (pos + 2 < s.length() && eqAt(s, pos, ',')
+					&& s.charAt(pos + 1) == ' ') {
+				pos += 2;
+			}
+		}
+		if (pos >= s.length()) {
+			throw new IllegalArgumentException("Error parsing list");
+		}
+		// skip '}'
+		pos += 1;
+		return pos;
+	}
+
+	protected int readKey(Stack stack, String s, int pos) {
+		int posEnd = pos;
+		while (posEnd < s.length() && !eqAt(s, posEnd, '=')) {
+			posEnd++;
+		}
+		if (posEnd == s.length()) {
+			throw new IllegalArgumentException("Error parsing map key: "
+					+ s.substring(pos));
+		}
+		stack.push(s.substring(pos, posEnd));
+		return posEnd;
+	}
+
+	protected int readList(Stack stack, String s, int pos) {
+		if (s.charAt(pos) != '[') {
+			throw new IllegalArgumentException("Error parsing list: "
+					+ s.substring(pos));
+		}
+		List list = new ArrayList();
+		stack.push(list);
+		// skip '['
+		pos += 1;
+		while (pos < s.length() && !eqAt(s, pos, ']')) {
+			pos = readObject(stack, s, pos);
+			list.add(stack.pop());
+			// skip ", "
+			if (pos + 2 < s.length() && eqAt(s, pos, ',')
+					&& s.charAt(pos + 1) == ' ') {
+				pos += 2;
+				if (eqAt(s, pos, ']')) {
+					list.add("");
+				}
+			}
+		}
+		if (pos >= s.length()) {
+			throw new IllegalArgumentException("Error parsing list");
+		}
+		// skip ']'
+		pos += 1;
+		return pos;
+	}
+
+	protected String unescape(String s, int startPos, int endPos) {
+		if (s == null || s.length() == 0) {
+			return s;
+		}
+		StringBuffer sb = new StringBuffer(endPos - startPos);
+		for (int i = startPos; i < endPos; ++i) {
+			char c = s.charAt(i);
+			if (i + 1 < endPos && c == '\\') {
+				char c2 = s.charAt(i + 1);
+				if (c2 == '{' || c2 == '}' || c2 == '[' || c2 == ']'
+						|| c2 == ',') {
+					// do nothing, skip '\' character
+				} else {
+					sb.append(c);
+				}
+			} else {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
+
+	protected boolean eqAt(String s, int pos, char c) {
+		return s.charAt(pos) == c && (pos == 0 || s.charAt(pos - 1) != '\\');
+	}
+}
