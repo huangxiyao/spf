a9dbee9d3942f51c9b8bb72d3d263e67664b4bde
liuliyey <liuliyey@mycompany.com>
2008-12-23 08:09:18 +0000

    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/portlet-filter@158 fbe1f26e-0b55-418f-9102-102d79b04d0f

---
+++ src/main/java/com/hp/it/spf/sso/portlet/filter/MapAttributeFilter.java
@@ -0,0 +1,184 @@
+package com.hp.it.spf.sso.portlet.filter;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.EventRequest;
+import javax.portlet.EventResponse;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+import javax.portlet.filter.ActionFilter;
+import javax.portlet.filter.EventFilter;
+import javax.portlet.filter.FilterChain;
+import javax.portlet.filter.FilterConfig;
+import javax.portlet.filter.RenderFilter;
+import javax.portlet.filter.ResourceFilter;
+import javax.servlet.http.HttpServletRequest;
+
+public class MapAttributeFilter implements ActionFilter, RenderFilter,
+        EventFilter, ResourceFilter {
+    private FilterConfig filterConfig;
+
+    private static boolean mIsLocalPortlet = false;
+    static {
+        try {
+            // local if vap class found
+            String classVignette = "com.vignette.portal.portlet.jsrcontainer.PortletCommandServlet";
+            Class.forName(classVignette);
+            mIsLocalPortlet = true;
+        } catch (ClassNotFoundException e) {
+            mIsLocalPortlet = false;
+        }
+    }
+
+    // prefix of the attribute passed by VAP
+    private final String VIGNETTE_PREFIX = "com.vignette.portal.attribute.portlet.";
+
+    public void init(FilterConfig filterConfig) throws PortletException {
+        this.filterConfig = filterConfig;
+    }
+
+    public void destroy() {
+
+    }
+
+    public void doFilter(ActionRequest actionRequest,
+            ActionResponse actionResponse, FilterChain filterChain)
+            throws IOException, PortletException {
+        mapAtttributeFromPortalToPortlet(actionRequest);
+        filterChain.doFilter(actionRequest, actionResponse);
+    }
+
+    public void doFilter(RenderRequest renderRequest,
+            RenderResponse renderResponse, FilterChain filterChain)
+            throws IOException, PortletException {
+        mapAtttributeFromPortalToPortlet(renderRequest);
+        filterChain.doFilter(renderRequest, renderResponse);
+    }
+
+    public void doFilter(ResourceRequest resourceRequest,
+            ResourceResponse resourceResponse, FilterChain filterChain)
+            throws IOException, PortletException {
+        mapAtttributeFromPortalToPortlet(resourceRequest);
+        filterChain.doFilter(resourceRequest, resourceResponse);
+    }
+
+    public void doFilter(EventRequest eventRequest,
+            EventResponse eventResponse, FilterChain filterChain)
+            throws IOException, PortletException {
+        mapAtttributeFromPortalToPortlet(eventRequest);
+        filterChain.doFilter(eventRequest, eventResponse);
+    }
+
+    /**
+     * map all attribute passed by Portal to portlet Portal will dispatch each
+     * attributes to the related destination portlets, So each individual
+     * portlet will receive its own attributes
+     * 
+     * @param PortletRequest
+     *            ActionRequest, RenderRequest
+     */
+    private void mapAtttributeFromPortalToPortlet(PortletRequest request) {
+
+        if (mIsLocalPortlet) {
+            forVAP(request);
+        } else {
+            forOpenPortal(request);
+
+            modifyLocale(request);
+        }
+    }
+
+    /**
+     * map all attribute passed by Portal to portlet in vignette
+     * 
+     * @param PortletRequest
+     *            ActionRequest, RenderRequest
+     */
+    private void forVAP(PortletRequest request) {
+        // retrieve all attribute names start with VIGNETTE_PREFIX
+        Enumeration enums = request.getAttributeNames();
+        while(enums.hasMoreElements()) {
+            String attKey = (String)enums.nextElement();
+            if (attKey.startsWith(VIGNETTE_PREFIX)) {
+                String localKey = attKey.replaceFirst(VIGNETTE_PREFIX, "");
+                // ignore the portlet name, as the portlet will receive
+                // its own attributes dispatched by VAP
+                int start = localKey.indexOf(".");
+                localKey = localKey.substring(start + 1);
+
+                request.setAttribute(localKey, request.getAttribute(attKey));
+            }
+        }
+    }
+
+    /**
+     * map all attribute passed by Portal to portlet in OpenPortal
+     * 
+     * @param PortletRequest
+     *            ActionRequest, RenderRequest
+     */
+    private void forOpenPortal(PortletRequest request) {
+        HttpServletRequest rq = (HttpServletRequest)request
+                .getAttribute("javax.portlet.portletc.httpServletRequest");
+        if (rq.getAttribute("com.hp.spp.UserProfile") instanceof Map) {
+            Object obj = rq.getAttribute("portlet_container_request");
+            if (obj != null) {
+                try {
+                    Method method = obj.getClass().getMethod("setUserInfo",
+                            new Class[] { Map.class });
+                    method.invoke(obj, new Object[] { (Map)rq
+                            .getAttribute("com.hp.spp.UserProfile") });
+                } catch (SecurityException e) {
+                } catch (NoSuchMethodException e) {
+                } catch (IllegalArgumentException e) {
+                } catch (IllegalAccessException e) {
+                } catch (InvocationTargetException e) {
+                }
+            }
+        }
+    }
+
+    /**
+     * OpenPortal incorrectly create the locale passed from Consumer by SOAP
+     */
+    private void modifyLocale(PortletRequest request) {
+        Locale currLocale = request.getLocale();
+        if (currLocale != null) {
+            String[] locale = currLocale.toString().split("[-_]");
+            Locale excatLocale = Locale.getDefault();
+            if (locale.length > 1) {
+                excatLocale = new Locale(locale[0], locale[1]);
+            } else {
+                excatLocale = new Locale(locale[0]);
+            }
+            HttpServletRequest rq = (HttpServletRequest)request
+                    .getAttribute("javax.portlet.portletc.httpServletRequest");
+            Object obj = rq.getAttribute("portlet_container_request");
+            if (obj != null) {
+                try {
+                    Field field = (Field)obj.getClass().getDeclaredField(
+                            "request");
+                    field.setAccessible(true);
+                    Method method = field.getType().getMethod("setLocale",
+                            new Class[] { Locale.class });
+                    method.invoke(field.get(obj), new Object[] { excatLocale });
+                    field.setAccessible(false);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
