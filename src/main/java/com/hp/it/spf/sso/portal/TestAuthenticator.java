2e7e629c0d6dda2267761ee4184ab6d37c4d7692
dingk <dingk@mycompany.com>
2009-01-06 05:15:07 +0000

    fix guest mode bug
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/session-initialization@203 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/sso/portal/TestAuthenticator.java
+++ src/main/java/com/hp/it/spf/sso/portal/TestAuthenticator.java
@@ -34,196 +34,206 @@ import com.epicentric.user.User;
  */
 public class TestAuthenticator extends AbstractAuthenticator {
 
-    private static final com.vignette.portal.log.LogWrapper LOG = AuthenticatorHelper
-            .getLog(TestAuthenticator.class);
+	private static final com.vignette.portal.log.LogWrapper LOG = AuthenticatorHelper
+			.getLog(TestAuthenticator.class);
 
-    private ResourceBundle prop;
+	private ResourceBundle prop;
 
-    private String profileFileName = null;
+	private String profileFileName = null;
 
-    private final String VIGNETTE_PREFIX = "com.vignette.portal.attribute.portlet.";
+	private final String VIGNETTE_PREFIX = "com.vignette.portal.attribute.portlet.";
 
-    /**
-     * Constructor for TestAuthenticator. It will get the resource bundle file
-     * for TestAuthenticator. Then it will read current user from the resource
-     * bundle file. The current user's info will be read from its property file
-     * as well.
-     * 
-     * @param request
-     *            HttpServletRequest object
-     */
-    public TestAuthenticator(HttpServletRequest request) {
-        this.request = request;
-        profileFileName = retrieveProfileFile();
-        try {
-            LOG.info("Get Resource Bundle File = " + profileFileName);
-            // Read current user's info.
-            prop = ResourceBundle.getBundle(profileFileName);
-            LOG
-                    .info("Refresh Resource Bundle File for user "
-                            + profileFileName);
-        } catch (MissingResourceException e) {
-            LOG.info("No Resource Bundle File = " + profileFileName);
-            request.getSession().setAttribute(
-                    AuthenticationConsts.SESSION_ATTR_SSO_ERROR, "1");
-        }
-    }
+	/**
+	 * Constructor for TestAuthenticator. It will get the resource bundle file
+	 * for TestAuthenticator. Then it will read current user from the resource
+	 * bundle file. The current user's info will be read from its property file
+	 * as well.
+	 * 
+	 * @param request
+	 *            HttpServletRequest object
+	 */
+	public TestAuthenticator(HttpServletRequest request) {
+		this.request = request;
+		profileFileName = retrieveProfileFile();
+		try {
+			LOG.info("Get Resource Bundle File = " + profileFileName);
+			// Read current user's info.
+			prop = ResourceBundle.getBundle(profileFileName);
+			LOG
+					.info("Refresh Resource Bundle File for user "
+							+ profileFileName);
+		} catch (MissingResourceException e) {
+			LOG.info("No Resource Bundle File = " + profileFileName);
+			request.getSession().setAttribute(
+					AuthenticationConsts.SESSION_ATTR_SSO_ERROR, "1");
+		}
+	}
 
-    private String retrieveProfileFile() {
-        String rbFile = retrieveRbFile();
-        // Get current user.
-        String currentUser = (String)request.getSession().getAttribute(
-                "sandbox.username");
-        try {
-            refreshBundle();
-            LOG.info("Get Resource Bundle File = " + rbFile);
-            rb = ResourceBundle.getBundle(rbFile);
-            if (currentUser == null) {
-                currentUser = rb.getString("CurrentUser");
-            }
-        } catch (MissingResourceException e) {
-            LOG.info("No Resource Bundle File = " + rbFile);
-        }
-        Site currentSite = getCurrentSite();
-        String currentSiteName = currentSite != null ? currentSite.getDNSName()
-                : "console";
-        return currentSiteName + "_" + currentUser;
-    }
+	private String retrieveProfileFile() {
+		String rbFile = retrieveRbFile();
+		// Get current user.
+		String currentUser = (String) request.getSession().getAttribute(
+				"sandbox.username");
+		try {
+			refreshBundle();
+			LOG.info("Get Resource Bundle File = " + rbFile);
+			rb = ResourceBundle.getBundle(rbFile);
+			if (currentUser == null) {
+				currentUser = rb.getString("CurrentUser");
+			}
+		} catch (MissingResourceException e) {
+			LOG.info("No Resource Bundle File = " + rbFile);
+		}
+		Site currentSite = getCurrentSite();
+		String currentSiteName = currentSite != null ? currentSite.getDNSName()
+				: "console";
+		return currentSiteName + "_" + currentUser;
+	}
 
-    /**
-     * Return current user's info from property file.
-     * 
-     * @param fieldName
-     *            field in request header
-     * @return corresponding field in request header
-     */
-    protected String getValue(String fieldName) {
-        if (fieldName == null || prop == null) {
-            return null;
-        }
-        try {
-            return prop.getString(fieldName);
-        } catch (java.util.MissingResourceException e) {
-            LOG.error(fieldName + " not found in properties file");
-            return null;
-        }
-    }
+	/**
+	 * Return current user's info from property file.
+	 * 
+	 * @param fieldName
+	 *            field in request header
+	 * @return corresponding field in request header
+	 */
+	protected String getValue(String fieldName) {
+		if (fieldName == null || prop == null) {
+			return null;
+		}
+		try {
+			return prop.getString(fieldName);
+		} catch (java.util.MissingResourceException e) {
+			LOG.error(fieldName + " not found in properties file");
+			return null;
+		}
+	}
 
-    /**
-     * refresh ResourceBundle cache to reload properties file content Otherwise
-     * we have to restart the server to refresh the resource bundle file
-     */
-    private void refreshBundle() {
-        try {
-            Class klass = java.util.ResourceBundle.class;
-            Field field = null;
-            try {
-                field = klass.getDeclaredField("cacheList");
-            } catch (NoSuchFieldException noSuchFieldEx) {
-                LOG.error(noSuchFieldEx);
-            }
-            // allow ourselves to manipulate the value of the cacheList
-            // (private) property in the ResourceBundle class
-            field.setAccessible(true);
-            sun.misc.SoftCache cache = null;
-            try {
-                cache = (sun.misc.SoftCache)field.get(null);
-            } catch (IllegalAccessException illegalAccessEx) {
-                LOG.error(illegalAccessEx);
-            }
-            cache.clear();
-            // Put back the private status on the cacheList property
-            field.setAccessible(false);
-        } catch (Exception e) {
-            LOG.error("Failed to refresh bundle due to " + e.getMessage());
-        }
-    }
+	/**
+	 * refresh ResourceBundle cache to reload properties file content Otherwise
+	 * we have to restart the server to refresh the resource bundle file
+	 */
+	private void refreshBundle() {
+		try {
+			Class klass = java.util.ResourceBundle.class;
+			Field field = null;
+			try {
+				field = klass.getDeclaredField("cacheList");
+			} catch (NoSuchFieldException noSuchFieldEx) {
+				LOG.error(noSuchFieldEx);
+			}
+			// allow ourselves to manipulate the value of the cacheList
+			// (private) property in the ResourceBundle class
+			field.setAccessible(true);
+			sun.misc.SoftCache cache = null;
+			try {
+				cache = (sun.misc.SoftCache) field.get(null);
+			} catch (IllegalAccessException illegalAccessEx) {
+				LOG.error(illegalAccessEx);
+			}
+			cache.clear();
+			// Put back the private status on the cacheList property
+			field.setAccessible(false);
+		} catch (Exception e) {
+			LOG.error("Failed to refresh bundle due to " + e.getMessage());
+		}
+	}
 
-    /**
-     * Get group info and return as a Set
-     * 
-     * @return groups
-     */
-    protected Set retrieveGroup() {
-        Set groups = new HashSet();
-        String groupstring = getValue(AuthenticationConsts.HEADER_GROUP_NAME);
-        // groups are divided by ,
-        if (groupstring != null) {
-            StringTokenizer st = new StringTokenizer(groupstring, ",");
-            while(st.hasMoreElements()) {
-                String group = (String)st.nextElement();
-                LOG.info("Get UserGroup = " + group);
-                groups.add(group);
-            }
-        }
-        return groups;
-    }
+	/**
+	 * Get group info and return as a Set
+	 * 
+	 * @return groups
+	 */
+	protected Set retrieveGroup() {
+		Set groups = new HashSet();
+		String groupstring = getValue(AuthenticationConsts.HEADER_GROUP_NAME);
+		// groups are divided by ,
+		if (groupstring != null) {
+			StringTokenizer st = new StringTokenizer(groupstring, ",");
+			while (st.hasMoreElements()) {
+				String group = (String) st.nextElement();
+				LOG.info("Get UserGroup = " + group);
+				groups.add(group);
+			}
+		}
+		return groups;
+	}
 
-    /**
-     * Prepare infos for userProfile Map
-     * 
-     * @return customizedProfile map
-     */
-    protected Map getCustomizedProfile() {
-        Map customizedProfile = new HashMap();
-        Enumeration propertyKeys = prop.getKeys();
-        while(propertyKeys.hasMoreElements()) {
-            String customizedProfileKey = (String)propertyKeys.nextElement();
-            customizedProfile.put(customizedProfileKey, prop
-                    .getString(customizedProfileKey));
-        }
-        return customizedProfile;
-    }
+	/**
+	 * Prepare infos for userProfile Map
+	 * 
+	 * @return customizedProfile map
+	 */
+	protected Map getCustomizedProfile() {
+		Map customizedProfile = new HashMap();
+		Enumeration propertyKeys = prop.getKeys();
+		while (propertyKeys.hasMoreElements()) {
+			String customizedProfileKey = (String) propertyKeys.nextElement();
+			customizedProfile.put(customizedProfileKey, prop
+					.getString(customizedProfileKey));
+		}
+		return customizedProfile;
+	}
 
-    /**
-     * This method is used to perform all related tasks. 1. It will invoke the
-     * mapRequest2User() method to map all the information from SSO product to
-     * SSOUser object. 2. Invoke the syncUser() method to perform the sync from
-     * SSOUser object to VAP. 3. Set the correct user name
-     * 
-     * @see com.hp.it.spf.sso.portal.IAuthenticator#execute()
-     */
-    public void execute() {
-        // guestMode is for logout
-        if (!"true".equalsIgnoreCase(request.getParameter("guestMode"))) {
-            super.execute();
-            boolean islocalMode = "true"
-                    .equalsIgnoreCase(getProperty("local_mode"));
-            String portlets = getProperty("portlets");
-            // portlets are divided by ,
-            if (islocalMode && portlets != null) {
-                StringTokenizer st = new StringTokenizer(portlets, ",");
-                while(st.hasMoreElements()) {
-                    String portlet = (String)st.nextElement();
-                    // pass userProfile to portlets in vignette way
-                    request.setAttribute(VIGNETTE_PREFIX + portlet
-                            + ".javax.portlet.userinfo", request.getSession()
-                            .getAttribute(USER_PROFILE_KEY));
-                }
-            }
-            User user = SessionUtils.getCurrentUser(request.getSession());
-            if (user != null) {
-                // make up locale for user
-                String country = ssoUser.getCountry();
-                String language = ssoUser.getLanguage();
-                Locale locale = Locale.ENGLISH;
-                if (language != null) {
-                    locale = country != null ? new Locale(language, country)
-                            : new Locale(language);
-                }
-                if (!user.isGuestUser()) {
-                    I18nUtils.setUserLocale(user, locale);
-                }
-            }
-        } else {
-            request.getSession().removeAttribute(USER_PROFILE_KEY);
-            // cleanupsession() will only clean session attributes starting from
-            // sp_
-            AuthenticatorHelper.cleanupSession(request);
-            userName = null;
-            Localizer localizer = I18nUtils.getLocalizer(request
-                    .getSession(false), request);
-            localizer.setLocale(Locale.ENGLISH);
-        }
-    }
+	/**
+	 * This method is used to perform all related tasks. 1. It will invoke the
+	 * mapRequest2User() method to map all the information from SSO product to
+	 * SSOUser object. 2. Invoke the syncUser() method to perform the sync from
+	 * SSOUser object to VAP. 3. Set the correct user name
+	 * 
+	 * @see com.hp.it.spf.sso.portal.IAuthenticator#execute()
+	 */
+	public void execute() {
+		// first time login or initSession
+		if (request.getSession().getAttribute("active") == null
+				|| "true".equalsIgnoreCase((String) request
+						.getParameter("initSession"))) {
+			request.getSession().setAttribute("active", "true");
+		}
+		boolean active = "true".equals(request.getSession().getAttribute(
+				"active"));
+		// guestMode is for logout
+		if ("true".equalsIgnoreCase(request.getParameter("guestMode"))) {
+			request.getSession().removeAttribute(USER_PROFILE_KEY);
+			// cleanupsession() will only clean session attributes starting from
+			// sp_
+			AuthenticatorHelper.cleanupSession(request);
+			request.getSession().setAttribute("active", "false");
+			userName = null;
+			Localizer localizer = I18nUtils.getLocalizer(request
+					.getSession(false), request);
+			localizer.setLocale(Locale.ENGLISH);
+		} else if (active) {
+			// not guest
+			super.execute();
+			boolean islocalMode = "true"
+					.equalsIgnoreCase(getProperty("local_mode"));
+			String portlets = getProperty("portlets");
+			// portlets are divided by ,
+			if (islocalMode && portlets != null) {
+				StringTokenizer st = new StringTokenizer(portlets, ",");
+				while (st.hasMoreElements()) {
+					String portlet = (String) st.nextElement();
+					// pass userProfile to portlets in vignette way
+					request.setAttribute(VIGNETTE_PREFIX + portlet
+							+ ".javax.portlet.userinfo", request.getSession()
+							.getAttribute(USER_PROFILE_KEY));
+				}
+			}
+			User user = SessionUtils.getCurrentUser(request.getSession());
+			if (user != null) {
+				// make up locale for user
+				String country = ssoUser.getCountry();
+				String language = ssoUser.getLanguage();
+				Locale locale = Locale.ENGLISH;
+				if (language != null) {
+					locale = country != null ? new Locale(language, country)
+							: new Locale(language);
+				}
+				if (!user.isGuestUser()) {
+					I18nUtils.setUserLocale(user, locale);
+				}
+			}
+		}
+	}
 }
