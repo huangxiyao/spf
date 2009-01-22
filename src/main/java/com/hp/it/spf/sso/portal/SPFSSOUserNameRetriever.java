2a8d5803203cbc03949e12ca2199a228a46820de
liuliyey <liuliyey@mycompany.com>
2009-01-22 12:18:53 +0000

    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/session-initialization@684 fbe1f26e-0b55-418f-9102-102d79b04d0f

---
+++ src/main/java/com/hp/it/spf/sso/portal/SPFSSOUserNameRetriever.java
@@ -0,0 +1,42 @@
+/*
+ * Project: Shared Portal Framework
+ * Copyright (c) 2008 HP. All Rights Reserved.
+ *
+ */
+package com.hp.it.spf.sso.portal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.epicentric.authentication.SSOUsernameRetriever;
+import com.vignette.portal.log.LogWrapper;
+
+/**
+ * This class implements SSOUsernameRetriever interface and returns a valid SSO user name
+ * defined in session. Vignette will call getSSOUsername method in this class to
+ * implement SSO
+ * 
+ * @author <link href="kaijian.ding@hp.com">dingk</link>,  		
+ * 		   <link href="ying-zhiw@hp.com">Oliver</link>
+ * @version TBD
+ */
+public class SPFSSOUserNameRetriever implements SSOUsernameRetriever {
+    private static final LogWrapper LOG = AuthenticatorHelper.getLog(SPFSSOUserNameRetriever.class);
+
+    /**
+     * @param request
+     *            HttpServletRequest object
+     * @return userName
+     * @see com.epicentric.authentication.SSOUsernameRetriever#getSSOUsername(javax.servlet.http.HttpServletRequest)
+     */
+    public String getSSOUsername(HttpServletRequest request) {
+		String ssoUsername = null;
+		HttpSession session = request.getSession(true);
+		try {
+		    ssoUsername = (String) session.getAttribute(AuthenticationConsts.SSO_USERNAME);
+		} catch (Exception ex) {
+		    LOG.debug("Can't find SSO username which must be already defined in http session, maybe this is a guest user");
+		}
+		return ssoUsername;
+    }
+}
