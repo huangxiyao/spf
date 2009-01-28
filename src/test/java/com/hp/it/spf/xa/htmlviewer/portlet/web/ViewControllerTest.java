6ee17b0f4e153f0b93b61ae86eae67405b4b4012
djorgen <djorgen@mycompany.com>
2009-01-28 15:51:03 +0000

    Added another test case.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/spf-portlet-utilities@837 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/test/java/com/hp/it/spf/xa/htmlviewer/portlet/web/ViewControllerTest.java
+++ src/test/java/com/hp/it/spf/xa/htmlviewer/portlet/web/ViewControllerTest.java
@@ -4,6 +4,7 @@
  */
 package com.hp.it.spf.xa.htmlviewer.portlet.web;
 
+import java.io.File;
 import java.util.Locale;
 import java.util.Map;
 import javax.portlet.PortletPreferences;
@@ -24,78 +25,117 @@ import com.hp.it.spf.xa.htmlviewer.portlet.web.ViewController;
  */
 public class ViewControllerTest extends TestCase {
 
-    private ViewController viewController;
+	private ViewController viewController;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see junit.framework.TestCase#setUp()
-     */
-    protected void setUp() throws Exception {
-        viewController = new ViewController();
-        viewController.setViewName("view");
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		viewController = new ViewController();
+		viewController.setViewName("view");
+	}
 
-    /**
-     * Test get relative html name.
-     * 
-     * @throws Exception the exception
-     */
-    public void testGetRelativeHtmlName() throws Exception {
-        MockRenderRequest renderRequest = new MockRenderRequest();
-        PortletPreferences pp = renderRequest.getPreferences();
-        pp.setValue(Consts.VIEW_FILENAME, "test.html");
-        assertEquals("/html/test.html", viewController.getFilename(renderRequest));
-    }
+	/**
+	 * Test get relative html name.
+	 * 
+	 * @throws Exception
+	 *             the exception
+	 */
+	public void testGetRelativeHtmlName() throws Exception {
+		MockRenderRequest renderRequest = new MockRenderRequest();
+		PortletPreferences pp = renderRequest.getPreferences();
+		pp.setValue(Consts.VIEW_FILENAME, "test.html");
+		assertEquals("/html/test.html", viewController
+				.getFilename(renderRequest));
+	}
 
-    /**
-     * Test execute.
-     * 
-     * @throws Exception the exception
-     */
-    public void testExecute() throws Exception {
-        MockRenderRequest renderRequest = new MockRenderRequest();
-        MockRenderResponse renderResponse = new MockRenderResponse();
-        ModelAndView modelAndView = (ModelAndView) viewController.execute(
-                renderRequest, renderResponse, "file content");
-        assertEquals("view", modelAndView.getViewName());
-        Map map = modelAndView.getModel();
-        String content = (String) map.get(Consts.VIEW_CONTENT);
-        assertEquals("file content", content);
-    }
+	/**
+	 * Test execute.
+	 * 
+	 * @throws Exception
+	 *             the exception
+	 */
+	public void testExecute() throws Exception {
+		MockRenderRequest renderRequest = new MockRenderRequest();
+		MockRenderResponse renderResponse = new MockRenderResponse();
+		ModelAndView modelAndView = (ModelAndView) viewController.execute(
+				renderRequest, renderResponse, "file content");
+		assertEquals("view", modelAndView.getViewName());
+		Map map = modelAndView.getModel();
+		String content = (String) map.get(Consts.VIEW_CONTENT);
+		assertEquals("file content", content);
+	}
 
-    /**
-     * Test handle render request internal.
-     * 
-     * @throws Exception the exception
-     */
-    public void testHandleRenderRequestInternal() throws Exception {
-        MockRenderRequest renderRequest = new MockRenderRequest();
-        MockRenderResponse renderResponse = new MockRenderResponse();
-        renderRequest.addPreferredLocale(new Locale("zh", "CN"));
-        PortletPreferences pp = renderRequest.getPreferences();
-        pp.setValue(Consts.VIEW_FILENAME, "test_basic.html");
-        ModelAndView modelAndView = (ModelAndView) viewController
-                .handleRenderRequest(renderRequest, renderResponse);
-        assertEquals("view", modelAndView.getViewName());
-        Map map = modelAndView.getModel();
-        String content = (String) map.get(Consts.VIEW_CONTENT);
-        System.out.println("testHandleRenderRequestInternal.1 got: " + content);
-        assertEquals("<html><head><title>Hello world (Chinese)!</title></head><body><h1>Hello world (Chinese)!</h1></body></html>",
-                content);
-        pp.setValue(Consts.VIEW_FILENAME, "test_loc_content_url_1.html");
-        modelAndView = (ModelAndView) viewController.handleRenderRequest(renderRequest, renderResponse);
-        map = modelAndView.getModel();
-        content = (String) map.get(Consts.VIEW_CONTENT);
-        System.out.println("testHandleRenderRequestInternal.2 got: " + content);
-        assertEquals("<html><head><title>Hello world (Chinese)!</title></head><body><h1>Hello world (Chinese)!</h1> Here is an image tag: <img src=\"/images/test_zh.gif\"></body></html>",
-                content);
-        pp.setValue(Consts.VIEW_FILENAME, "test_loc_content_url_2.html");
-        modelAndView = (ModelAndView) viewController.handleRenderRequest(renderRequest, renderResponse);
-        map = modelAndView.getModel();
-        content = (String) map.get(Consts.VIEW_CONTENT);
-        System.out.println("testHandleRenderRequestInternal.3 got: " + content);
-        assertEquals("<html><head><title>Hello world!</title></head><body><h1>Hello world!</h1> Here is an image tag: <img src=\"/images/nonexistent.gif\"></body></html>",
-                content);        
-    }
+	/**
+	 * Test handle render request internal.
+	 * 
+	 * @throws Exception
+	 *             the exception
+	 */
+	public void testHandleRenderRequestInternal() throws Exception {
+		MockRenderRequest renderRequest = new MockRenderRequest();
+		MockRenderResponse renderResponse = new MockRenderResponse();
+		renderRequest.addPreferredLocale(new Locale("zh", "CN"));
+		PortletPreferences pp = renderRequest.getPreferences();
+		pp.setValue(Consts.VIEW_FILENAME, "test_basic.html");
+		ModelAndView modelAndView = (ModelAndView) viewController
+				.handleRenderRequest(renderRequest, renderResponse);
+		assertEquals("view", modelAndView.getViewName());
+		Map map = modelAndView.getModel();
+		String content = (String) map.get(Consts.VIEW_CONTENT);
+		System.out.println("testHandleRenderRequestInternal.1 got: " + content);
+		assertEquals(
+				"<html><head><title>Hello world (Chinese)!</title></head><body><h1>Hello world (Chinese)!</h1></body></html>",
+				content);
+		pp.setValue(Consts.VIEW_FILENAME, "test_loc_content_url_1.html");
+		modelAndView = (ModelAndView) viewController.handleRenderRequest(
+				renderRequest, renderResponse);
+		map = modelAndView.getModel();
+		content = (String) map.get(Consts.VIEW_CONTENT);
+		System.out.println("testHandleRenderRequestInternal.2 got: " + content);
+		assertEquals(
+				"<html><head><title>Hello world (Chinese)!</title></head><body><h1>Hello world (Chinese)!</h1> Here is an image tag: <img src=\"/images/test_zh.gif\"></body></html>",
+				content);
+		pp.setValue(Consts.VIEW_FILENAME, "test_loc_content_url_2.html");
+		modelAndView = (ModelAndView) viewController.handleRenderRequest(
+				renderRequest, renderResponse);
+		map = modelAndView.getModel();
+		content = (String) map.get(Consts.VIEW_CONTENT);
+		System.out.println("testHandleRenderRequestInternal.3 got: " + content);
+		assertEquals(
+				"<html><head><title>Hello world!</title></head><body><h1>Hello world!</h1> Here is an image tag: <img src=\"/images/nonexistent.gif\"></body></html>",
+				content);
+		if (fileExists("/opt/sasuapps/spf/globalResources/portlet/i18n/html/test_loc_content_url_ext_zh_CN.html")
+				&& fileExists("/opt/sasuapps/spf/globalResources/portlet/i18n/images/test_ext_zh_CN.gif")) {
+			pp.setValue(Consts.VIEW_FILENAME, "test_loc_content_url_ext.html");
+			modelAndView = (ModelAndView) viewController.handleRenderRequest(
+					renderRequest, renderResponse);
+			map = modelAndView.getModel();
+			content = (String) map.get(Consts.VIEW_CONTENT);
+			System.out.println("testHandleRenderRequestInternal.4 got: "
+					+ content);
+			assertEquals(
+					"<html><head><title>Hello world (Chinese)!</title></head><body><h1>Hello world (Chinese)!</h1> Here is an image tag: <img src=\"/relay/images/test_ext_zh_CN.gif\"></body></html>",
+					content);
+		}
+	}
+
+	protected static boolean fileExists(String pPath) {
+		boolean result = false;
+		if (pPath == null) {
+			pPath = "";
+		}
+		pPath = pPath.trim();
+		try {
+			File file = new File(pPath);
+			if (file.exists()) {
+				result = true;
+			}
+		} catch (Exception e) {
+			// Exception (eg file not found) - ignore and return false.
+		}
+		return result;
+	}
 }
