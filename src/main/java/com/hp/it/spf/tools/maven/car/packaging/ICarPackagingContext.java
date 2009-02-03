280aca7d05afd33814a389915e037706f852baec
bdehamer <bdehamer@mycompany.com>
2009-02-03 18:44:37 +0000

    Added API documentation to the packaging interface classes
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/tools/trunk/spf-maven-car-plugin@937 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/tools/maven/car/packaging/ICarPackagingContext.java
+++ src/main/java/com/hp/it/spf/tools/maven/car/packaging/ICarPackagingContext.java
@@ -7,49 +7,130 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.archiver.jar.JarArchiver;
 
+
+/**
+ * Encapsulates all of the Maven project and CAR plugin configuration data that
+ * may be needed by the various packaging tasks.
+ * 
+ * @see ICarPackagingTask
+ * @author bdehamer
+ * @since 1.0
+ */
 public interface ICarPackagingContext
 {
     /**
-     * Returns the maven project.
+     * Returns the Maven project object.
      *
      * @return the project
      */
     public MavenProject getProject();
     
+    
     /**
-     * Returns the logger to use to output logging event.
+     * Returns the project logger.
      *
      * @return the logger
      */
     Log getLog();
     
     
+    /**
+     * Returns the <code>File</code> object representing the source directory
+     * for the web application resources.
+     * 
+     * @return the source directory for the web application resource files
+     */
     public File getWebAppSourceDir();
     
     
+    /**
+     * Returns the <code>File</code> object representing the target directory
+     * where all of the CAR resources will be assembled before they are archived.
+     * 
+     * @return the staging directory for the CAR resources
+     */
     public File getWebAppOutputDir();
     
     
+    /**
+     * Returns the <code>File</code> object representing the directory
+     * where the compiled Java class files to be included as part of the
+     * final CAR are located.
+     * 
+     * @return the target directory for compiled Java class files
+     */
     public File getClassesOutputDir();
     
     
+    /**
+     * Returns a boolean flag indicating whether or not archiving is enabled for
+     * Java classes. A value of <code>true</code> indicates that Java class
+     * files should be combined into a JAR before being included in the final
+     * CAR. A vlaue of <code>false</code> indicates that Java classes should be
+     * packaged as loose files in the <em>WEB-INF/classes/</em> directory of the
+     * final CAR.
+     * 
+     * @return a flag indicating whether or not Java classes should be archived before
+     * being included in the CAR
+     */
     public boolean isClassArchivingEnabled();
-    
-    
+
+
+    /**
+     * Returns a boolean flag indicating whether or not the CAR being assembled
+     * is a composite CAR. A value of <code>true</code> indicates that a
+     * composite CAR is being assembled while a value of <code>false</code>
+     * indicates that this is not a composite CAR.
+     * 
+     * @return a flag indicating whether or not a composite CAR is being
+     * assembled.
+     */
     public boolean isComposite();
     
     
+    /**
+     * Returns the <code>JarArchiver</code> to be used for archiving Java class
+     * files into a JAR.
+     * 
+     * @see ICarPackagingContext#isClassArchivingEnabled()
+     * @return the <code>JarArchiver</code>
+     */
     public JarArchiver getJarArchiver();
     
     
+    /**
+     * Returns the archive configuration to be used when archiving Java
+     * class files into a JAR.
+     * 
+     * @see ICarPackagingContext#isClassArchivingEnabled()
+     * @return the archive configuration object
+     */
     public MavenArchiveConfiguration getArchiveConfiguration();
     
     
+    /**
+     * Return the file name mapping pattern to be used when including
+     * other artifacts as part of the final CAR.
+     * 
+     * @return the file name mapping pattern
+     */
     public String getOutputFileNameMapping();
     
     
+    /**
+     * Returns the list of tokens to include when copying content
+     * from the webapp source directory.
+     * 
+     * @return the list of tokens to include when copying webapp files
+     */
     public String[] getIncludes();
     
-    
+
+    /**
+     * Returns the list of tokens to exclude when copying content
+     * from the webapp source directory.
+     * 
+     * @return the list of tokens to exclude when copying webapp files
+     */    
     public String[] getExcludes();
 }
--- src/main/java/com/hp/it/spf/tools/maven/car/packaging/ICarPackagingTask.java
+++ src/main/java/com/hp/it/spf/tools/maven/car/packaging/ICarPackagingTask.java
@@ -3,9 +3,32 @@ package com.hp.it.spf.tools.maven.car.packaging;
 import org.apache.maven.plugin.MojoExecutionException;
 
 
+/**
+ * Common interface for all packaging tasks used as part of the
+ * {@link com.hp.it.spf.tools.maven.car.CarMojo CarMojo}. The process of
+ * assembling a complete component archive (CAR) requires a number of distinct
+ * packaging tasks like copying class files, copying dependencies, etc.
+ * <code>ICarPackaingTask</code> serves as the common interface for all of the
+ * concrete packaging implementation classes that may be invoked as part of the
+ * process of assembling the final CAR.
+ * 
+ * @author bdehamer
+ * @since 1.0
+ */
 public interface ICarPackagingTask
 {
 
+    /**
+     * Process the packaging request. This will almost always result in one or
+     * more files being copied to the target/ directory of the Maven project.
+     * There is nothing to return.
+     * 
+     * @param context
+     * the packaging context containing the necessary configuration data
+     * 
+     * @throws MojoExecutionException
+     * in case of any errors during the packaging process
+     */
     public void doPackaging(ICarPackagingContext context) throws MojoExecutionException;
 
 }
