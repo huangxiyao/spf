d020274851e2dbe8c9c2c974844bfc670257e106
liuliyey <liuliyey@mycompany.com>
2009-02-08 04:47:44 +0000

    move the jms cache coordination from pc-database-persistence to here
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/spf-portlet-container@1115 fbe1f26e-0b55-418f-9102-102d79b04d0f

---
+++ src/main/java/com/hp/it/spf/openportal/portletcontainer/context/registry/database/JMSCacheCoordinator.java
@@ -0,0 +1,54 @@
+package com.hp.it.spf.openportal.portletcontainer.context.registry.database;
+
+import java.net.InetAddress;
+
+import org.eclipse.persistence.config.SessionCustomizer;
+import org.eclipse.persistence.sessions.DatabaseSession;
+import org.eclipse.persistence.sessions.Session;
+import org.eclipse.persistence.sessions.coordination.CommandProcessor;
+import org.eclipse.persistence.sessions.coordination.RemoteCommandManager;
+import org.eclipse.persistence.sessions.coordination.jms.JMSTopicTransportManager;
+
+import com.hp.it.spf.xa.misc.Environment;
+
+public class JMSCacheCoordinator implements SessionCustomizer{
+
+	public void customize(Session session) throws Exception {
+		// define the Command Manager
+        RemoteCommandManager commandMgr = new RemoteCommandManager((CommandProcessor) session);
+        
+        // define the JMS transport
+        JMSTopicTransportManager transportMgr = new JMSTopicTransportManager(commandMgr);
+        
+        StringBuilder hostUrl = new StringBuilder("t3://");
+        String host = InetAddress.getLocalHost().getHostName();
+        String port = String.valueOf(Environment.singletonInstance.getManagedServerPort());
+        //String port = "8002";
+        hostUrl.append(host).append(":").append(port);
+        System.out.println("Set TopicHostUrl = " + hostUrl.toString());
+        transportMgr.setTopicHostUrl(hostUrl.toString());
+        
+        transportMgr.setTopicConnectionFactoryName("jms/WSRPTopicConnectionFactory");
+        transportMgr.setTopicName("jms/EclipseLinkCacheTopic");
+        transportMgr.setUserName("weblogic");
+        transportMgr.setPassword("weblogic");
+
+        // Initial context factory name is default to 
+        // com.evermind.server.rmi.RMIInitialContextFactory for OracleAS
+        transportMgr.setInitialContextFactoryName("weblogic.jndi.WLInitialContextFactory");
+
+        // hook up the components
+        commandMgr.setTransportManager(transportMgr);
+        ((DatabaseSession)session).setCommandManager(commandMgr);
+        
+        // turn on distributed cache synchronization with RCM
+        ((DatabaseSession)session).setShouldPropagateChanges(true);        
+
+        // Start joining the cluster
+        if(session.isConnected ()) {
+        	((DatabaseSession)session).getCommandManager().initialize();
+        } else {
+        	((DatabaseSession)session).login();
+        }
+	}
+}
