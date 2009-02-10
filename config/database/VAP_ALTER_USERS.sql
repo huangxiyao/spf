9477ac9b23c3be4d8d16a43a3d235209bb2144e6
liuliyey <liuliyey@mycompany.com>
2009-02-10 01:07:18 +0000

    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/tools/trunk/spf-portal-tools@1163 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- config/database/VAP_ALTER_USERS.sql
+++ config/database/VAP_ALTER_USERS.sql
@@ -3,14 +3,14 @@
 * fields that are supported by SPF
 *  
 * Date Crated: Feb/4/2009
-* Date Last Modified: Feb/4/2009
+* Date Last Modified: Feb/10/2009
 * Created by : Liu, Ye
 ****************************************************************/
 
 ALTER TABLE USERS ADD PROFILE_ID VARCHAR(64) NULL;
 ALTER TABLE USERS ADD LAST_CHANGE_DATE DATE NULL;
 ALTER TABLE USERS ADD LAST_LOGIN_DATE DATE NULL;
-ALTER TABLE USERS ADD SP_TIMEZONE VARCHAR(64) NULL;
+ALTER TABLE USERS ADD SPF_TIMEZONE VARCHAR(64) NULL;
 
 commit;
 
