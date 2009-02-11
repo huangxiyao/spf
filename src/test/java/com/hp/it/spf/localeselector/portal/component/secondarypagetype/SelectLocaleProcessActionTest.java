487992d50973869d7fd098a596a9fedef5a772de
djorgen <djorgen@mycompany.com>
2009-02-11 19:51:07 +0000

    Added println's to test cases for debug.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/comps/trunk/spf-locale-selector-secondarypagetype@1190 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/test/java/com/hp/it/spf/localeselector/portal/component/secondarypagetype/SelectLocaleProcessActionTest.java
+++ src/test/java/com/hp/it/spf/localeselector/portal/component/secondarypagetype/SelectLocaleProcessActionTest.java
@@ -14,16 +14,20 @@ public class SelectLocaleProcessActionTest extends TestCase {
 	public void testFilterUrlLocaleParams() {
 		String expected = "/portal/site/test?aparam=dd";
 		String actual = action.filterUrlLocaleParams("/portal/site/test?aparam=dd&lang=en&cc=US");
+		System.out.println("testFilterUrlLocaleParams.1 got: " + actual);
 		assertEquals(expected, actual);
 		actual = action.filterUrlLocaleParams("/portal/site/test?lang=en&cc=US&aparam=dd");
+		System.out.println("testFilterUrlLocaleParams.2 got: " + actual);
 		assertEquals(expected, actual);
 		actual = action.filterUrlLocaleParams("/portal/site/test?lang=en&aparam=dd");
+		System.out.println("testFilterUrlLocaleParams.3 got: " + actual);
 		assertEquals(expected, actual);
 	}
 	
 	public void testFilterUrlLocaleParamsWhenNoParams() {
 		String expected = "/portal/site/test";
 		String actual = action.filterUrlLocaleParams("/portal/site/test");
+		System.out.println("testFilterUrlLocaleParamsWhenNoParams.1 got: " + actual);
 		assertEquals(expected, actual);
 	}
 
