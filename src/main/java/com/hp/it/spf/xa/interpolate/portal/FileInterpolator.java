1b989fdc696811a54ee70aa3a5d100257ef81048
djorgen <djorgen@mycompany.com>
2009-02-14 20:34:52 +0000

    Fixed bug in class JavaDoc.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/spf-portal-utilities@1252 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/xa/interpolate/portal/FileInterpolator.java
+++ src/main/java/com/hp/it/spf/xa/interpolate/portal/FileInterpolator.java
@@ -59,7 +59,7 @@ import com.vignette.portal.website.enduser.PortalContext;
  * tokens for a date range (ie content that should be included only in-between
  * certain dates). The <code><i>date</i></code> must adhere to the
  * {@link java.text.SimpleDateFormat} pattern in
- * {@link com.hp.it.spf.xa.interpolate.TokenParse#DATE_FORMAT}, which at this
+ * {@link com.hp.it.spf.xa.interpolate.TokenParser#DATE_FORMAT}, which at this
  * writing is the following:</p:>
  * 
  * <code><i>M</i>/<i>d</i>/<i>yyyy</i> <i>h</i>:<i>mm</i>:<i>ss</i> <i>a</i> <i>z</i></code>
@@ -101,6 +101,7 @@ import com.vignette.portal.website.enduser.PortalContext;
  *   {/BEFORE}
  * {/AFTER}
  * </pre>
+ * 
  * </p>
  * </dd>
  * 
