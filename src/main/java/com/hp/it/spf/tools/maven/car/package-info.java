ff4e0f9dc7a526ab14c5d9d0ecd922ffdf6ae8f4
bdehamer <bdehamer@mycompany.com>
2009-02-17 23:18:07 +0000

    Removing old package docs
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/tools/trunk/spf-maven-car-plugin@1296 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/tools/maven/car/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides Mojo implementations used as part of the Maven CAR plugin
- * 
- * @since 1.0
- */
-package com.hp.it.spf.tools.maven.car;
--- src/main/java/com/hp/it/spf/tools/maven/car/packaging/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Provides classes which support the various packaging activities required as part
- * of the CAR assembly process.
- */
-package com.hp.it.spf.tools.maven.car.packaging;
