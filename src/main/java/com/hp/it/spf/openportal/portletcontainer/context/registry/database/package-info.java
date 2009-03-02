201ee2e7a08bdf3f9f91ac4265021534cbe0cad1
liuliyey <liuliyey@mycompany.com>
2009-03-02 08:02:08 +0000

    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/spf-portlet-container@1614 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/openportal/portletcontainer/context/registry/database/package-info.java
+++ src/main/java/com/hp/it/spf/openportal/portletcontainer/context/registry/database/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Provides SPF customized features:
- * 1. a factory to support database persistence;
- * 2. use weblogic JMS to support cache coordination.
+ * 1) a factory to support database persistence;
+ * 2) use weblogic JMS to support cache coordination.
  */
 
 package com.hp.it.spf.openportal.portletcontainer.context.registry.database;
