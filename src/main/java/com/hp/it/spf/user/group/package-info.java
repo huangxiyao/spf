c1f9dcb03680c38d2f8aba63e934eee69bda3198
liuliyey <liuliyey@mycompany.com>
2009-03-02 07:28:03 +0000

    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/spf-session-initialization@1606 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/user/group/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains UGS web service client stub, utility classes regarding to retrieving user groups.
- */
-package com.hp.it.spf.user.group;
--- src/main/java/com/hp/it/spf/user/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package defines the logic for retrieveing user profiles and groups inform.
- */
-package com.hp.it.spf.user;
