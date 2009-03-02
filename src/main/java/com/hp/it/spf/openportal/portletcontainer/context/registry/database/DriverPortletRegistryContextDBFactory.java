984853273715a0da3597b67e726cdd0e9ee213fa
dingk <dingk@mycompany.com>
2009-03-02 10:07:15 +0000

    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/spf-portlet-container@1617 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/openportal/portletcontainer/context/registry/database/DriverPortletRegistryContextDBFactory.java
+++ src/main/java/com/hp/it/spf/openportal/portletcontainer/context/registry/database/DriverPortletRegistryContextDBFactory.java
@@ -1,29 +1,53 @@
+/*
+ * Project: Shared Portal Framework
+ * Copyright (c) 2008 HP. All Rights Reserved.
+ *
+ */
 package com.hp.it.spf.openportal.portletcontainer.context.registry.database;
 
 import com.sun.portal.portletcontainer.context.registry.PortletRegistryContext;
 import com.sun.portal.portletcontainer.context.registry.PortletRegistryContextFactory;
 import com.sun.portal.portletcontainer.context.registry.PortletRegistryException;
 
-public class DriverPortletRegistryContextDBFactory implements PortletRegistryContextFactory {
-	
-	private static String implementation = "com.hp.it.spf.openportal.portletcontainer.admin.database.PortletRegistryContextDBImpl";
-	
-	/**
-     * Returns a new instance of the Portlet Registry
-     */    
-    public PortletRegistryContext getPortletRegistryContext() throws PortletRegistryException {        
-        Object classInstance = null;
-        try {
-            classInstance = Class.forName(implementation).newInstance();
-        } catch (Exception ex) {
-            throw new PortletRegistryException(ex);
-        }
-        PortletRegistryContext portletRegistryContext = (PortletRegistryContext) classInstance;
-        
-        return portletRegistryContext;        
-    }
+/**
+ * A factory class to create concrete DriverPortletRegistryContext implementation
+ * 
+ * @author Ding, Kai-Jian
+ * 
+ */
+public class DriverPortletRegistryContextDBFactory implements
+		PortletRegistryContextFactory {
 
-    public PortletRegistryContext getPortletRegistryContext(String portalId, String namespace) throws PortletRegistryException {
-        return getPortletRegistryContext();
-    }
+	private static String implementation = "com.hp.it.spf.openportal.portletcontainer.admin.database.PortletRegistryContextDBImpl";
+
+	/**
+	 * Returns a new instance of the Portlet Registry
+	 */
+	public PortletRegistryContext getPortletRegistryContext()
+			throws PortletRegistryException {
+		Object classInstance = null;
+		try {
+			classInstance = Class.forName(implementation).newInstance();
+		} catch (Exception ex) {
+			throw new PortletRegistryException(ex);
+		}
+		PortletRegistryContext portletRegistryContext = (PortletRegistryContext) classInstance;
+
+		return portletRegistryContext;
+	}
+
+	/**
+	 * get portlet registry context
+	 * 
+	 * @param String
+	 *            portalId
+	 * @param String
+	 *            namespace
+	 * 
+	 * @return PortletRegistryContext
+	 */
+	public PortletRegistryContext getPortletRegistryContext(String portalId,
+			String namespace) throws PortletRegistryException {
+		return getPortletRegistryContext();
+	}
 }
