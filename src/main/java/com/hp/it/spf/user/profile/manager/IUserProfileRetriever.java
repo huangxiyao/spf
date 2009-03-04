27e822bef4d83978bd4ee18eac2f74b5434d0e52
wuyingzh <wuyingzh@mycompany.com>
2009-03-04 09:25:44 +0000

    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/spf-session-initialization@1653 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/user/profile/manager/IUserProfileRetriever.java
+++ src/main/java/com/hp/it/spf/user/profile/manager/IUserProfileRetriever.java
@@ -23,13 +23,11 @@ public interface IUserProfileRetriever {
     /**
      * Retrieve specified user profiles
      * 
-     * @param EUserIdentifierType user type
      * @param userIdentifier user indentifier
      * @param request HttpServletRequest
      * @return user profile map or an empty map
      * @throws UserProfileException if retrieving user profiles errror
      */
-    Map<String, String> getUserProfile(EUserIdentifierType userIdentifierType,
-                                       String profileId,
+    Map<String, String> getUserProfile(String profileId,
                                        HttpServletRequest request) throws UserProfileException;
 }
