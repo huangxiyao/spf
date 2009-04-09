a977dd793393cd6ae067facd97fb529fa3033869
Kaijian Ding <kaijian.ding@hp.com>
2009-04-09 09:49:39 +0000

    fix a bug about guest user
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/xa/trunk/spf-portal-session@2603 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/sso/portal/TestAuthenticator.java
+++ src/main/java/com/hp/it/spf/sso/portal/TestAuthenticator.java
@@ -20,7 +20,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import com.epicentric.common.website.I18nUtils;
-import com.epicentric.common.website.Localizer;
 import com.epicentric.common.website.SessionUtils;
 import com.epicentric.site.Site;
 import com.epicentric.user.User;
@@ -46,63 +45,92 @@ public class TestAuthenticator extends AbstractAuthenticator {
 
 	private final String MESSAGEPANE = "messagePane";
 
-    /**
-     * Constructor for TestAuthenticator. It will get the resource bundle file
-     * for TestAuthenticator. Then it will read current user from the resource
-     * bundle file. The current user's info will be read from its property file
-     * as well.
-     * 
-     * @param request
-     *            HttpServletRequest object
-     */
-    public TestAuthenticator(HttpServletRequest request) {
-        this.request = request;
+	private final String GUESTUSER = "guestuser";
 
-        String profileFileName = retrieveProfileFile();
-        try {
-            LOG.info("Get Resource Bundle File = " + profileFileName);
-            // Read current user's info.
-            prop = ResourceBundle.getBundle(profileFileName);
-            LOG.info("Refresh Resource Bundle File for user " + profileFileName + ".properties");
-        } catch (MissingResourceException e) {
-            LOG.info("No Resource Bundle File = " + profileFileName);
-            StringBuffer message = new StringBuffer("Cound not find " + profileFileName + ".properties");
-            String currentUser = (String) request.getSession().getAttribute("sandbox.username");
-			request.getSession().setAttribute("sandbox.username", rb.getString("CurrentUser"));
-			String profileFileName2 = retrieveProfileFile();
-            LOG.info("Get Resource Bundle File = " + profileFileName2);
-            // Read current user's info.
-            prop = ResourceBundle.getBundle(profileFileName2);
-            LOG.info("Refresh Resource Bundle File for user " + profileFileName2);
-            message.append(", use " + profileFileName2 + ".properties (default one) instead.");
-            message.append(" please create " + profileFileName + ".properties for user ");
-            message.append(currentUser);
-    		Site currentSite = AuthenticatorHelper.getCurrentSite(request);
-    		String currentSiteName = currentSite != null ? Utils.getEffectiveSite(request).getDNSName(): "console";
-            message.append(" on " + currentSiteName + " site in %domain_home%/sandbox_resoureces/ directory.");
+	private final String SANDBOX_USERNAME = "sandbox.username";
+
+	private boolean actAsANON = false;
+
+	/**
+	 * Constructor for TestAuthenticator. It will get the resource bundle file
+	 * for TestAuthenticator. Then it will read current user from the resource
+	 * bundle file. The current user's info will be read from its property file
+	 * as well.
+	 * 
+	 * @param request
+	 *            HttpServletRequest object
+	 */
+	public TestAuthenticator(HttpServletRequest request) {
+		this.request = request;
+		// clean previous message pane
+		request.getSession().removeAttribute(MESSAGEPANE);
+
+		String profileFileName = retrieveProfileFile();
+		try {
+			LOG.info("Get Resource Bundle File = " + profileFileName);
+			// Read current user's info.
+			prop = ResourceBundle.getBundle(profileFileName);
+			LOG.info("Refresh Resource Bundle File for user " + profileFileName
+					+ ".properties");
+		} catch (MissingResourceException e) {
+			LOG.info("No Resource Bundle File = " + profileFileName);
+			StringBuffer message = new StringBuffer("Cound not find "
+					+ profileFileName + ".properties.");
+			String currentUser = (String) request.getSession().getAttribute(
+					SANDBOX_USERNAME);
+			Site currentSite = AuthenticatorHelper.getCurrentSite(request);
+			String profileFileName2 = "console_" + rb.getString("CurrentUser");
+			// if not going to console, use anon user as logged in user
+			if (currentSite != null) {
+				actAsANON = true;
+			}
+			LOG.info("Get Resource Bundle File = " + profileFileName2);
+			// Read current user's info.
+			prop = ResourceBundle.getBundle(profileFileName2);
+			LOG.info("Refresh Resource Bundle File for user "
+					+ profileFileName2);
+			message.append(" Please create " + profileFileName
+					+ ".properties for user ");
+			message.append(currentUser);
+			String currentSiteName = currentSite != null ? Utils
+					.getEffectiveSite(request).getDNSName() : "console";
+			message.append(" on " + currentSiteName
+					+ " site in %domain_home%/sandbox_resoureces/ directory.");
 			request.getSession().setAttribute(MESSAGEPANE, message.toString());
-        }
-    }
+		}
+	}
 
 	private String retrieveProfileFile() {
 		String rbFile = retrieveRbFile();
 		// Get current user.
-		String currentUser = (String) request.getSession().getAttribute("sandbox.username");
+		String currentUser = request.getParameter("username");
+		if (currentUser == null || "".equals(currentUser.trim())) {
+			currentUser = (String) request.getSession().getAttribute(
+					SANDBOX_USERNAME);
+		}
 		try {
 			refreshBundle();
 			LOG.info("Get Resource Bundle File = " + rbFile);
 			rb = ResourceBundle.getBundle(rbFile);
-			if (currentUser == null || "".equals(currentUser.trim())) {
-				currentUser = rb.getString("CurrentUser");
+			if (currentUser == null || "".equals(currentUser.trim())
+					|| GUESTUSER.equals(currentUser)) {
+				// in this kind, act as anon user
+				if (Utils.getEffectiveSite(request) != null) {
+					actAsANON = true;
+					return "console_" + rb.getString("CurrentUser");
+				} else {
+					currentUser = rb.getString("CurrentUser");
+				}
 			}
 		} catch (MissingResourceException e) {
 			LOG.info("No Resource Bundle File = " + rbFile);
 		}
 		if (Utils.getEffectiveSite(request) == null) {
-		    return "console_" + currentUser;
+			return "console_" + currentUser;
 		}
 		Site currentSite = AuthenticatorHelper.getCurrentSite(request);
-		String currentSiteName = currentSite != null ? Utils.getEffectiveSiteDNS(request): "console";
+		String currentSiteName = currentSite != null ? Utils
+				.getEffectiveSiteDNS(request) : "console";
 		return currentSiteName + "_" + currentUser;
 	}
 
@@ -155,110 +183,103 @@ public class TestAuthenticator extends AbstractAuthenticator {
 		}
 	}
 
-    /**
-     * Get group info and return as a Set
-     * 
-     * @return groups
-     */
-    protected Set getUserGroup() {
-        Set groups = new HashSet();
-        String groupstring = getValue(AuthenticationConsts.HEADER_GROUP_NAME);
-        // groups are divided by ,
-        if (groupstring != null) {
-            StringTokenizer st = new StringTokenizer(groupstring, ",");
-            while(st.hasMoreElements()) {
-                String group = (String)st.nextElement();
-                LOG.info("Get UserGroup = " + group);
-                groups.add(group);
-            }
-        }
-        // retrieve groups with invoking super method and merge them
-        groups.addAll(super.getUserGroup());
-        return groups;
-    }
-
-    /**
-     * Prepare infos for userProfile Map
-     * 
-     * @return customizedProfile map
-     */
-    protected Map getUserProfile() {
-        Map customizedProfile = new HashMap();
-        Enumeration propertyKeys = prop.getKeys();
-        while(propertyKeys.hasMoreElements()) {
-            String customizedProfileKey = (String)propertyKeys.nextElement();
-            customizedProfile.put(customizedProfileKey, prop
-                    .getString(customizedProfileKey));
-        }
-        return customizedProfile;
-    }
-
-    protected void mapHeaderToUserProfileMap() {   
-        // retrieve user profile
-        userProfile = getUserProfile();  
-    }
-    
 	/**
-	 * This method is used to perform all related tasks. 1. It will invoke the
-	 * mapRequest2User() method to map all the information from SSO product to
-	 * SSOUser object. 2. Invoke the syncUser() method to perform the sync from
-	 * SSOUser object to VAP. 3. Set the correct user name
+	 * Get group info and return as a Set
+	 * 
+	 * @return groups
+	 */
+	protected Set getUserGroup() {
+		Set groups = new HashSet();
+		String groupstring = getValue(AuthenticationConsts.HEADER_GROUP_NAME);
+		// groups are divided by ,
+		if (groupstring != null) {
+			StringTokenizer st = new StringTokenizer(groupstring, ",");
+			while (st.hasMoreElements()) {
+				String group = (String) st.nextElement();
+				LOG.info("Get UserGroup = " + group);
+				groups.add(group);
+			}
+		}
+		// retrieve groups with invoking super method and merge them
+		groups.addAll(super.getUserGroup());
+		return groups;
+	}
+
+	/**
+	 * Prepare infos for userProfile Map
+	 * 
+	 * @return customizedProfile map
+	 */
+	protected Map getUserProfile() {
+		Map customizedProfile = new HashMap();
+		Enumeration propertyKeys = prop.getKeys();
+		while (propertyKeys.hasMoreElements()) {
+			String customizedProfileKey = (String) propertyKeys.nextElement();
+			customizedProfile.put(customizedProfileKey, prop
+					.getString(customizedProfileKey));
+		}
+		return customizedProfile;
+	}
+
+	protected void mapHeaderToUserProfileMap() {
+		// retrieve user profile
+		userProfile = getUserProfile();
+	}
+
+	/**
+	 * This method is used to perform all related tasks. 
+	 * 1. If act as anon user
+	 * 2. act as normal user
+	 * 3. pass user info to portlet
+	 * 4. pass context map to portlet
 	 * 
 	 * @see com.hp.it.spf.sso.portal.IAuthenticator#execute()
 	 */
 	public void execute() {
-		String guestMode = request.getParameter("guestMode");
-		// if there is any error
-		// first time login or initSession
-		if (request.getSession().getAttribute("active") == null
-				|| "true".equalsIgnoreCase((String) request
-						.getParameter("initSession"))) {
-			request.getSession().setAttribute("active", "true");
+		if ("true".equalsIgnoreCase(request.getParameter("guestMode"))) {
+			actAsANON = true;
 		}
-		boolean active = "true".equals(request.getSession().getAttribute(
-				"active"));
-		// guestMode is for logout
-		if ("true".equalsIgnoreCase(guestMode)) {
-			Localizer localizer = I18nUtils.getLocalizer(request
-					.getSession(false), request);
-			localizer.setLocale(Locale.ENGLISH);
+		// guestMode
+		if (actAsANON == true) {
 			ANONAuthenticator authenticator = new ANONAuthenticator(request);
 			authenticator.execute();
 			userName = authenticator.getUserName();
-			request.getSession().setAttribute("active", "false");
-		} else if (active) {
+			request.getSession().setAttribute(SANDBOX_USERNAME, GUESTUSER);
+		} else {
 			// not guest
 			super.execute();
-			boolean islocalMode = "true"
-					.equalsIgnoreCase(getProperty("local_mode"));
-			String portlets = getProperty("portlets");
-			// portlets are divided by ,
-			if (islocalMode && portlets != null) {
-				StringTokenizer st = new StringTokenizer(portlets, ",");
-				while (st.hasMoreElements()) {
-					String portlet = (String) st.nextElement();
-					// pass userProfile to portlets in vignette way
-					request.setAttribute(VIGNETTE_PREFIX + portlet
-							+ ".javax.portlet.userinfo", request.getSession()
-							.getAttribute(AuthenticationConsts.USER_PROFILE_KEY));
-					// pass userProfile to portlets in vignette way
-					request.setAttribute(VIGNETTE_PREFIX + portlet
-							+ "." + Consts.PORTAL_CONTEXT_KEY, retrieveUserContextKeys(request));
-				}
+			request.getSession().setAttribute(SANDBOX_USERNAME, userName);
+		}
+		boolean islocalMode = "true"
+				.equalsIgnoreCase(getProperty("local_mode"));
+		String portlets = getProperty("portlets");
+		// portlets are divided by ,
+		if (islocalMode && portlets != null) {
+			StringTokenizer st = new StringTokenizer(portlets, ",");
+			while (st.hasMoreElements()) {
+				String portlet = (String) st.nextElement();
+				// pass userProfile to portlets in vignette way
+				request.setAttribute(VIGNETTE_PREFIX + portlet
+						+ ".javax.portlet.userinfo", request.getSession()
+						.getAttribute(AuthenticationConsts.USER_PROFILE_KEY));
+				// pass userProfile to portlets in vignette way
+				request.setAttribute(VIGNETTE_PREFIX + portlet + "."
+						+ Consts.PORTAL_CONTEXT_KEY,
+						retrieveUserContextKeys(request));
 			}
-			User user = SessionUtils.getCurrentUser(request.getSession());
-			if (user != null) {
-				// make up locale for user
-				String country = ssoUser.getCountry();
-				String language = ssoUser.getLanguage();
-				Locale locale = Locale.ENGLISH;
-				if (language != null) {
-					locale = country != null ? new Locale(language, country)
-							: new Locale(language);
-				}
-				if (!user.isGuestUser()) {
-					I18nUtils.setUserLocale(user, locale);
-				}
+		}
+		User user = SessionUtils.getCurrentUser(request.getSession());
+		if (user != null) {
+			// make up locale for user
+			String country = ssoUser.getCountry();
+			String language = ssoUser.getLanguage();
+			Locale locale = Locale.ENGLISH;
+			if (language != null) {
+				locale = country != null ? new Locale(language, country)
+						: new Locale(language);
+			}
+			if (!user.isGuestUser()) {
+				I18nUtils.setUserLocale(user, locale);
 			}
 		}
 	}
@@ -268,12 +289,17 @@ public class TestAuthenticator extends AbstractAuthenticator {
 	 *            user original request
 	 * @return user context key map whose values are {@link String} objects
 	 */
-	private Map<String, String> retrieveUserContextKeys(HttpServletRequest request) {
+	private Map<String, String> retrieveUserContextKeys(
+			HttpServletRequest request) {
 		Map<String, String> userContext = new HashMap<String, String>();
-		userContext.put(Consts.KEY_PORTAL_SITE_URL, Utils.getPortalSiteURL(request));
-		userContext.put(Consts.KEY_PORTAL_REQUEST_URL, Utils.getRequestURL(request));
-		userContext.put(Consts.KEY_PORTAL_SITE_NAME, Utils.getEffectiveSiteDNS(request));
-		userContext.put(Consts.KEY_PORTAL_SESSION_ID, request.getSession(true).getId());
+		userContext.put(Consts.KEY_PORTAL_SITE_URL, Utils
+				.getPortalSiteURL(request));
+		userContext.put(Consts.KEY_PORTAL_REQUEST_URL, Utils
+				.getRequestURL(request));
+		userContext.put(Consts.KEY_PORTAL_SITE_NAME, Utils
+				.getEffectiveSiteDNS(request));
+		userContext.put(Consts.KEY_PORTAL_SESSION_ID, request.getSession(true)
+				.getId());
 		userContext.put(Consts.KEY_SESSION_TOKEN, getHppSessionToken(request));
 		return userContext;
 	}
