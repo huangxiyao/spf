6c6db3b3c40c52f2176eed3d8aaeffe23266a4a9
Scott Jorgenson <scott.jorgenson@hp.com>
2009-06-17 04:17:24 +0000

    Added JSR 168 resource URL support to PortalURL stuff.  This is most of the fix for QC CR# 72.  The other part is in PortalURLRewriterFilter in spf-portal-filters (not yet done at time of this writing).
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk/spf-common-utilities@3296 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- src/main/java/com/hp/it/spf/xa/portalurl/PortalURL.java
+++ src/main/java/com/hp/it/spf/xa/portalurl/PortalURL.java
@@ -13,6 +13,7 @@ import java.util.Map;
  * portlet public render parameters.
  * 
  * @author Slawek Zachcial (slawomir.zachcial@hp.com)
+ * @author Scott Jorgenson (scott.jorgenson@hp.com)
  */
 public interface PortalURL {
 
@@ -93,14 +94,17 @@ public interface PortalURL {
 	 * and same <tt>portletFriendlyId</tt>, use the companion
 	 * {@link #setParameter(String, String, String[])} method.
 	 * <p>
-	 * <b>Note:</b> This method is for setting portlet <i>private</i> 
+	 * <b>Note:</b> This method is for setting portlet <i>private</i>
 	 * parameters. To pass <i>public</i> render parameters to portlets instead,
 	 * use a method like {@link #setPublicParameter(String, String, String)}.
 	 * To pass <i>portal</i> query parameters, use a method like
 	 * {@link #setParameter(String, String)}.
-	 * 
-	 * The private parameters will be set as action parameters when action URL is created,
-	 * and this is only relevant to the remote portlets.
+	 * <p>
+	 * <b>Note:</b> This parameter will be set as an action parameter when an
+	 * action URL is set (only in the case of remote portlets). If a resource
+	 * URL is set, it will be set as a resource URL parameter (in the case of a
+	 * JSR-286 implementation - for a JSR-168 implementation, results are
+	 * undefined).
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
@@ -127,9 +131,12 @@ public interface PortalURL {
 	 * use a method like {@link #setPublicParameter(String, String, String)}.
 	 * To pass <i>portal</i> query parameters, use a method like
 	 * {@link #setParameter(String, String)}.
-	 * 
-	 * The private parameters will be set as action parameters when action URL is created,
-	 * and this is only relevant to the remote portlets.
+	 * <p>
+	 * <b>Note:</b> These parameters will be set as action parameters when an
+	 * action URL is set (only in the case of remote portlets). If a resource
+	 * URL is set, they will be set as resource URL parameters (in the case of a
+	 * JSR-286 implementation - for a JSR-168 implementation, results are
+	 * undefined).
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
@@ -156,14 +163,17 @@ public interface PortalURL {
 	 * method, any previous value(s) set for the same <tt>paramName</tt> and
 	 * same <tt>portletFriendlyId</tt> will be overwritten.
 	 * <p>
-	 * <b>Note:</b> This method is for setting portlet <i>private</i> 
+	 * <b>Note:</b> This method is for setting portlet <i>private</i>
 	 * parameters. To pass <i>public</i> render parameters to portlets instead,
 	 * use a method like {@link #setPublicParameter(String, String, String)}.
 	 * To pass <i>portal</i> query parameters, use a method like
 	 * {@link #setParameter(String, String)}.
-	 * 
-	 * The private parameters will be set as action parameters when action URL is created,
-	 * and this is only relevant to the remote portlets.
+	 * <p>
+	 * <b>Note:</b> These parameters will be set as action parameters when an
+	 * action URL is set (only in the case of remote portlets). If a resource
+	 * URL is set, they will be set as resource URL parameters (in the case of a
+	 * JSR-286 implementation - for a JSR-168 implementation, results are
+	 * undefined).
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
@@ -190,8 +200,9 @@ public interface PortalURL {
 	 * instead, use a method like {@link #setParameter(String, String, String)}.
 	 * To pass <i>portal</i> query parameters, use a method like
 	 * {@link #setParameter(String, String)}.
-	 * 
-	 * The use of these parameters when creating action URL is "undefined and may lead to unexpected results" 
+	 * <p>
+	 * Use of this method is undefined and may produce unexpected results, if
+	 * you set your <code>PortalURL</code> to be a resource URL.
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
@@ -219,8 +230,9 @@ public interface PortalURL {
 	 * instead, use a method like {@link #setParameter(String, String, String)}.
 	 * To pass <i>portal</i> query parameters, use a method like
 	 * {@link #setParameter(String, String)}.
-	 * 
-	 * The use of these parameters when creating action URL is "undefined and may lead to unexpected results"
+	 * <p>
+	 * Use of this method is undefined and may produce unexpected results, if
+	 * you set your <code>PortalURL</code> to be a resource URL.
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
@@ -248,8 +260,9 @@ public interface PortalURL {
 	 * instead, use a method like {@link #setParameter(String, String, String)}.
 	 * To pass <i>portal</i> query parameters, use a method like
 	 * {@link #setParameter(String, String)}.
-	 * 
-	 * The use of these parameters when creating action URL is "undefined and may lead to unexpected results"
+	 * <p>
+	 * Use of this method is undefined and may produce unexpected results, if
+	 * you set your <code>PortalURL</code> to be a resource or action URL.
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
@@ -264,6 +277,9 @@ public interface PortalURL {
 	 * Sets a <tt>windowState</tt> targeted at the portlet with the given
 	 * <tt>portletFriendlyId</tt>. If the given <tt>windowState</tt> is
 	 * null, then {@link javax.portlet.WindowState#NORMAL} is presumed.
+	 * <p>
+	 * Use of this method is undefined and may produce unexpected results, if
+	 * you set your <code>PortalURL</code> to be a resource URL.
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
@@ -277,6 +293,9 @@ public interface PortalURL {
 	 * Sets a <tt>portletMode</tt> targeted at the portlet with the given
 	 * <tt>portletFriendlyId</tt>. If the given <tt>portletMode</tt> is
 	 * null, then {@link javax.portlet.PortletMode#VIEW} is presumed.
+	 * <p>
+	 * Use of this method is undefined and may produce unexpected results, if
+	 * you set your <code>PortalURL</code> to be a resource URL.
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
@@ -287,20 +306,78 @@ public interface PortalURL {
 
 	/**
 	 * Makes this <code>PortalURL</code> an action URL targeted at the portlet
-	 * with the given <tt>portletFriendlyId</tt>. You can only do this once
-	 * with any one <code>PortalURL</code> object, since setting an action URL
-	 * irreversibly changes the state of the object.
-	 * 
-	 * The private parameters will be set as action parameters when action URL is created,
-	 * and this is only relevant to the remote portlets.
+	 * with the given <tt>portletFriendlyId</tt>. You can use either this or
+	 * {@link #setAsResourceURL(String, String)} at most once with any one
+	 * <code>PortalURL</code> object, since setting an action URL or resource
+	 * URL irreversibly changes the state of the object.
+	 * <p>
+	 * Note that when you use this method to make this <code>portalURL</code>
+	 * be an action URL, any private parameters you set (eg using
+	 * {@link #setParameter(String, String, String)}) will be treated as action
+	 * parameters (only in the case of remote portlets). Any public parameters
+	 * you set (eg using {@link #setPublicParameter(String, String, String)})
+	 * will have undefined results.
 	 * 
 	 * @param portletFriendlyId
 	 *            the portlet friendly ID as configured in the portal console
 	 * @throws IllegalStateException
-	 *             if action URL has already been set
+	 *             if action or resource URL has already been set
 	 * @since SPF 1.0
 	 */
 	void setAsActionURL(String portletFriendlyId) throws IllegalStateException;
+
+	/**
+	 * Makes this <code>PortalURL</code> a resource URL for the given resource
+	 * ID, targeted at the portlet with the given <tt>portletFriendlyId</tt>.
+	 * Implementations of this method may support JSR-286 or may only be JSR-168
+	 * compliant.
+	 * <p>
+	 * <ul>
+	 * <li>In the case of a JSR-286 implementation, the given resource ID would
+	 * be a resource ID string for identifying the resource within the portlet's
+	 * <code>serveResource</code> method.</li>
+	 * <li>In the case of a JSR-168 implementation, the given resource ID would
+	 * typically be a URL for a servlet, image, or other static file inside the
+	 * portlet application on the producer server.
+	 * <ul>
+	 * <li>A relative URL will be
+	 * assumed to be relative to the portal server, so provide an absolute URL
+	 * if your portlet may be remoted.</li>
+	 * <li>The resource ID must already contain
+	 * whatever parameters and formatting the resource needs - eg if the
+	 * resource is a servlet which expects to find request parameters in its
+	 * query string, you should add that query string to the resource ID before
+	 * passing it to this method.</li>
+	 * </ul>
+	 * </li>
+	 * </ul>
+	 * <p>
+	 * You can use either this method or {@link #setAsActionURL(String)} at most
+	 * once with any one <code>PortalURL</code> object, since setting a
+	 * resource URL or action URL changes the state of the object irreversibly.
+	 * <p>
+	 * Note that when you use this method to make a resource URL, any private
+	 * parameters you set (eg using
+	 * {@link #setParameter(String, String, String)}) will be treated as
+	 * resource parameters in the case of a JSR-286 implementation. In the case
+	 * of a JSR-168 implementation, results are undefined.
+	 * <p>
+	 * Any public parameters, portlet mode or window state you set (eg using
+	 * {@link #setPublicParameter(String, String, String)} or
+	 * {@link #setPortletMode(String, PortletMode)}) will have undefined
+	 * results.
+	 * 
+	 * @param portletFriendlyId
+	 *            the portlet friendly ID as configured in the portal console
+	 * @param resourceID
+	 *            the resource ID (JSR-286) or servlet or static resource URL
+	 *            (JSR-168) for the resource to serve at that portlet
+	 * @throws IllegalStateException
+	 *             if action or resource URL has already been set
+	 * @since SPF 1.1
+	 */
+	void setAsResourceURL(String portletFriendlyId, String resourceID)
+			throws IllegalStateException;
 
 	/**
 	 * Returns the <code>PortalURL</code> in String form.
--- src/main/java/com/hp/it/spf/xa/portalurl/VignetteRemotePortalURL.java
+++ src/main/java/com/hp/it/spf/xa/portalurl/VignetteRemotePortalURL.java
@@ -6,6 +6,8 @@ import java.util.Iterator;
 import java.net.URLEncoder;
 import java.io.UnsupportedEncodingException;
 
+import com.hp.it.spf.xa.portalurl.AbstractPortalURL.PortletParameters;
+
 /**
  * A concrete {@link PortalURL} which implements remote portlet handling for
  * portlet parameters.
@@ -17,6 +19,9 @@ class VignetteRemotePortalURL extends AbstractPortalURL {
 	private static final char NAV_STATE_ARRAY_DELIMITER = ';';
 	private static final char NAV_STATE_PARAM_DELIMITER = '|';
 
+	/**
+	 * Call the superclass constructor to build this object.
+	 */
 	protected VignetteRemotePortalURL(String siteRootUrl,
 			String anotherSiteName, String pageFriendlyUri, boolean secure,
 			int nonStandardHttpPort, int nonStandardHttpsPort) {
@@ -24,6 +29,9 @@ class VignetteRemotePortalURL extends AbstractPortalURL {
 				nonStandardHttpPort, nonStandardHttpsPort);
 	}
 
+	/**
+	 * Add private parameters in the format for Vignette local portlets.
+	 */
 	protected void addPrivateParameters(StringBuilder result,
 			Map.Entry<String, PortletParameters> portletParameters,
 			String portletFriendlyId, boolean isActionURL) {
@@ -86,6 +94,9 @@ class VignetteRemotePortalURL extends AbstractPortalURL {
 		}
 	}
 
+	/**
+	 * Add public parameters in the format for Vignette local portlets.
+	 */
 	protected void addPublicParameters(StringBuilder result,
 			Map.Entry<String, PortletParameters> portletParameters,
 			String portletFriendlyId) {
@@ -114,4 +125,120 @@ class VignetteRemotePortalURL extends AbstractPortalURL {
 		}
 	}
 
+	/**
+	 * Create a resource URL string for a Vignette remote portlet. This is a
+	 * JSR-168 implementation, in which a portlet resource URL is a servlet,
+	 * image or other static file typically in the portlet application. JSR-286
+	 * true portlet resource serving is not supported by this implementation.
+	 * <p>
+	 * A Vignette remote portlet resource URL contains the resource ID that was
+	 * set earlier (using
+	 * {@link AbstractPortalURL#setAsResourceURL(String, String)}), which was
+	 * either a relative or absolute HTTP or HTTPS URL. This method embeds that
+	 * in a Vignette <code>template.BINARYPORTLET</code> URL and returns it.
+	 * <p>
+	 * If the current <code>PortalURL</code> was not set as a resource URL,
+	 * this method returns empty.
+	 * 
+	 * @return the resource URL string for the remote Vignette portlet
+	 */
+	protected StringBuilder createResourceUrl() {
+
+		boolean isResourceUrl = mResourcePortletFriendlyId != null;
+		StringBuilder result = new StringBuilder();
+		if (!isResourceUrl) {
+			return (result);
+		}
+
+		// get the site root URL string and make sure it ends with '/'
+		result.append(createSiteRootUrl());
+		if (result.charAt(result.length() - 1) != '/') {
+			result.append('/');
+		}
+
+		// append the "template.BINARYPORTLET" secondary page friendly ID.
+		result.append("template.BINARYPORTLET/");
+
+		// for a resource URL, Vignette puts the friendly URL next (before the
+		// "resource.process")
+		if ((mPageFriendlyUri != null) && (mPageFriendlyUri.length() > 0)) {
+			if (mPageFriendlyUri.charAt(0) == '/') {
+				if (mPageFriendlyUri.length() > 1) {
+					result.append(mPageFriendlyUri.subSequence(1,
+							mPageFriendlyUri.length()));
+				}
+			} else {
+				result.append(mPageFriendlyUri);
+			}
+		}
+
+		// append the "resource.process" element next and
+		// terminate portal URL path with a "/" character
+		if (result.charAt(result.length() - 1) != '/') {
+			result.append('/');
+		}
+		result.append("resource.process/");
+
+		// merge and add portal parameters and portlet resource parameters
+		result.append(mergeQueryStrings(createPortalQueryString(),
+				createResourceQueryString()));
+
+		// finished creating resource URL
+		return (result);
+	}
+
+	private StringBuilder createResourceQueryString() {
+
+		boolean isResourceUrl = mResourcePortletFriendlyId != null;
+		StringBuilder result = new StringBuilder();
+		Iterator<Map.Entry<String, PortletParameters>> portletParameterEntries = mPortletParameters
+				.entrySet().iterator();
+
+		if (isResourceUrl) {
+			// if this is a resource URL we specify the target portlet for the
+			// resource (there is always only one) with ".tpst" parameter
+			// (I don't know what the "_ws_BI" means but Vignette generates it
+			// so
+			// we will put it in)
+			result.append('?');
+			result.append(PARAM_NAME_PREFIX).append(".tpst=").append(
+					mResourcePortletFriendlyId).append("_ws_BI");
+
+			// then we specify the resource information with ".rst_" parameter
+			result.append('&').append(PARAM_NAME_PREFIX).append(".rst_")
+					.append(mResourcePortletFriendlyId).append('=');
+
+			// first resource info is the WSRP URL
+			StringBuilder info = new StringBuilder();
+			try {
+				info.append("wsrp-url=");
+				info.append(URLEncoder.encode(mResourceId, "UTF-8"));
+				info.append('&');
+				info.append("wsrp-requiresRewrite=false");
+				result.append(URLEncoder.encode(info.toString(), "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				throw new RuntimeException(
+						"UTF-8 encoding not supported? " + e, e);
+			}
+
+			result.append("&javax.portlet.begCacheTok=com.vignette.cachetoken");
+			// we now will add the remaining portlet information if it is there,
+			// though it should not be (this may have undefined results when the
+			// URL is opened)
+			while (portletParameterEntries.hasNext()) {
+				Map.Entry<String, PortletParameters> portletParameters = portletParameterEntries
+						.next();
+				String portletFriendlyId = portletParameters.getKey();
+				addWindowStateAndPortletMode(result, portletFriendlyId,
+						portletParameters.getValue());
+				addPublicParameters(result, portletParameters,
+						portletFriendlyId);
+				addPrivateParameters(result, portletParameters,
+						portletFriendlyId, false);
+			}
+			result.append("&javax.portlet.endCacheTok=com.vignette.cachetoken");
+		}
+		return (result);
+
+	}
 }
