7be3bde8a1a1e199b0dadedb2dc22fec9f74ddab
Joey Sebestyen <joey.sebestyen@hp.com>
2009-12-03 15:53:14 +0000

    Attempts at causing Persona update to regenerate groups
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk@3837 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- portal/spf-portal-session/src/main/java/com/hp/it/spf/sso/portal/AbstractAuthenticator.java
+++ portal/spf-portal-session/src/main/java/com/hp/it/spf/sso/portal/AbstractAuthenticator.java
@@ -592,14 +592,20 @@ public abstract class AbstractAuthenticator implements IAuthenticator {
                 lastChangeDateInSession = ((Date)sessionUserProfile.get(AuthenticationConsts.KEY_LAST_CHANGE_DATE));
             }            
         }
+        
+        Map<String, Object> up = getUserProfile();
 
         // Judge
         if (lastChangeDate == null) {
             return false;
         } else if (lastChangeDateInSession == null) {
             return true;
-        } else {
-            return lastChangeDate.after(lastChangeDateInSession);
+        } else if( lastChangeDate.after(lastChangeDateInSession)){
+            return true;
+        } else if( null == up ){
+        		return true;
+       	} else {
+        	return !up.equals(sessionUserProfile);
         }
     }
 
