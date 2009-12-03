250d65b75f59b5ca670d3b089b70fcfad8ce0766
Slawomir Zachcial <slawomir.zachcial@hp.com>
2009-12-03 14:23:56 +0000

    Removing the requirement to have spf-common-utilities in the system classpath.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk/portlet@3835 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- spf-portlet-container/src/main/java/com/hp/it/spf/openportal/portletcontainer/context/registry/database/JMSCacheCoordinator.java
+++ spf-portlet-container/src/main/java/com/hp/it/spf/openportal/portletcontainer/context/registry/database/JMSCacheCoordinator.java
@@ -14,8 +14,10 @@ import org.eclipse.persistence.sessions.Session;
 import org.eclipse.persistence.sessions.coordination.CommandProcessor;
 import org.eclipse.persistence.sessions.coordination.RemoteCommandManager;
 import org.eclipse.persistence.sessions.coordination.jms.JMSTopicTransportManager;
-import com.hp.it.spf.xa.properties.PropertyResourceBundleManager;
-import com.hp.it.spf.xa.misc.Environment;
+
+import javax.management.ObjectName;
+import javax.management.MBeanServer;
+import javax.naming.InitialContext;
 
 /**
  * This class involves Weblogic JMS to support Eclipselink cache coordination.
@@ -24,6 +26,8 @@ import com.hp.it.spf.xa.misc.Environment;
  * @version TBD
  */
 public class JMSCacheCoordinator implements SessionCustomizer{
+
+	private int mManagedServerPort = -1;
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.persistence.config.SessionCustomizer#customize(org.eclipse.persistence.sessions.Session)
@@ -37,7 +41,7 @@ public class JMSCacheCoordinator implements SessionCustomizer{
         
         StringBuilder hostUrl = new StringBuilder("t3://");
         String host = InetAddress.getLocalHost().getHostName();
-        String port = String.valueOf(Environment.singletonInstance.getManagedServerPort());
+        String port = String.valueOf(getManagedServerPort());
         //String port = "8002";
         hostUrl.append(host).append(":").append(port);
         
@@ -45,8 +49,17 @@ public class JMSCacheCoordinator implements SessionCustomizer{
         
         transportMgr.setTopicConnectionFactoryName("jms/WSRPTopicConnectionFactory");
         transportMgr.setTopicName("jms/EclipseLinkCacheTopic");
-        
-        ResourceBundle rb = PropertyResourceBundleManager.getBundle("ContainerJMSConfig");
+
+		// Slawek's note:
+		// Previously this class referenced PropertyResourceBundleManager class defined in
+		// spf-common-utilities module. Unfortunately the resulting dependency required
+		// spf-common-utilities to be part of the system classpath this class must be in.
+		// This prevented portlet applications to use different versions of spf-common-utilities
+		// than the one referenced in the system classpath.
+		// The drawback of this change though is that PropertyResourceBundleManager was able
+		// to automatically detect changes of the underlying file and re-load it appropriately.
+		// Now such a change would require the server restart - let's hope that does not happen too often.
+        ResourceBundle rb = ResourceBundle.getBundle("ContainerJMSConfig");
         
         transportMgr.setUserName(rb.getString("UserName"));
         transportMgr.setPassword(rb.getString("Password"));
@@ -68,5 +81,34 @@ public class JMSCacheCoordinator implements SessionCustomizer{
         } else {
         	((DatabaseSession)session).login();
         }
+	}
+
+	/**
+	 * Retrieves this managed server's port number (ListenPort).
+	 *
+	 * <p><b>Note:</b>
+	 * The implementation of this method duplicates some code from <code>com.hp.it.spf.xa.misc.Environment</code>
+	 * class. The reason for this duplication is to avoid dependency on <code>spf-common-utilities</code>
+	 * hosting that class. Otherwise <code>spf-common-utilities</code> would have to be part of
+	 * system classpath as this class is and this would prevent portlet applications to bundle
+	 * a different version of this library than the one referenced in the classpath.
+	 * </p>
+	 *
+	 * @return  server's ListenPort
+	 * @throws Exception If an error occurs when WebLogic JMX objects are queries to retrieve
+	 * server listen port
+	 */
+	private synchronized int getManagedServerPort() throws Exception
+	{
+		if (mManagedServerPort == -1) {
+			ObjectName runtimeService =
+					new ObjectName("com.bea:Name=RuntimeService,Type=weblogic.management.mbeanservers.runtime.RuntimeServiceMBean");
+			InitialContext ctx = new InitialContext();
+			MBeanServer mbeanServer = (MBeanServer) ctx.lookup("java:comp/env/jmx/runtime");
+			ObjectName mServerRuntime = (ObjectName) mbeanServer.getAttribute(runtimeService, "ServerRuntime");
+			mManagedServerPort = ((Integer) mbeanServer.getAttribute(mServerRuntime, "ListenPort")).intValue();
+		}
+
+		return mManagedServerPort;
 	}
 }
