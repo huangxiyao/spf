5729dda422a807a1a82e41efb382fc6c42fc03bc
Scott Jorgenson <scott.jorgenson@hp.com>
2010-06-23 23:25:12 +0000

    Continued work on improved JavaDoc.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk/portal@4221 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- spf-portal-utilities/src/main/java/com/hp/it/spf/xa/i18n/portal/package-info.java
+++ spf-portal-utilities/src/main/java/com/hp/it/spf/xa/i18n/portal/package-info.java
@@ -1,21 +1,39 @@
 /**
- * <p>This package contains plain Java classes for internationalizing
- * Vignette portal components in Shared Portal Framework.</p>
- *
- * <a name="I18nUtility">
- * <h3>I18nUtility</h3>
- * </a>
- * <p>The {@link com.hp.it.spf.xa.i18n.portal.I18nUtility} class provides methods for:
+ * <p>
+ * This package contains plain Java classes for internationalizing Vignette
+ * portal components in Shared Portal Framework. 
+ * Please see the class documentation for more information on these classes.
+
+ * </p>
+ * 
+ * <a name="I18nUtility"> <h3>I18nUtility</h3> </a>
+ * <p>
+ * The {@link com.hp.it.spf.xa.i18n.portal.I18nUtility} class provides methods
+ * for:
  * <ul>
- *	<li>accessing message resources for your Vignette component (augmented with
- *	inherent SPF capabilities such as the <code>&lt;contextual_help&gt;...&lt;/contextual_help&gt;</code>
- *	and <code>&lt;no_localization&gt;...&lt;/no_localization&gt;</code>
- *	tokens)</li>
- *	<li>accessing secondary support files (eg static resource files) for your Vignette component</li>
- *	<li>getting locales configured for your Vignette portal site: all available locales, or just the default locale</li>
- *	<li>and more.</li>
+ * <li>accessing message resources for your Vignette component (augmented with
+ * inherent SPF capabilities such as the
+ * <code>&lt;contextual_help&gt;...&lt;/contextual_help&gt;</code> and
+ * <code>&lt;no_localization&gt;...&lt;/no_localization&gt;</code> tokens)</li>
+ * <li>accessing secondary support files (eg static resource files) for your
+ * Vignette component</li>
+ * <li>getting locales configured for your Vignette portal site: all available
+ * locales, or just the default locale</li>
+ * <li>and more.</li>
  * </ul>
- * Please see the class documentation for more information.
+ * </p>
+ * 
+ * <a name="ClassicLocaleSelectorProvider"> <h3>ClassicLocaleSelectorProvider</h3>
+ * </a>
+ * <p>
+ * The {@link com.hp.it.spf.xa.i18n.portal.ClassicLocaleSelectorProvider} class
+ * is a plain-Java class which builds and returns to you the HTML, CSS and JS
+ * markup and code for an HPWeb "classic"-style locale selector. (A JSP tag and
+ * even a Vignette component style are also provided for rendering the locale
+ * selector. These wrap around this <code>ClassicLocaleSelectorProvider</code>
+ * class. Please see the SPF portal utilities developer guide for more
+ * information.)
  * </p>
  */
 package com.hp.it.spf.xa.i18n.portal;
+
