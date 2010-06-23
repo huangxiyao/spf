9a7e87b6bd72ef10e0230118f065e874db1a6dac
Scott Jorgenson <scott.jorgenson@hp.com>
2010-06-23 15:56:47 +0000

    Started work on SPF portal utilities developer guide.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk@4220 fbe1f26e-0b55-418f-9102-102d79b04d0f

---
+++ portal/spf-portal-utilities/src/main/java/com/hp/it/spf/xa/i18n/portal/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * <p>This package contains plain Java classes for internationalizing
+ * Vignette portal components in Shared Portal Framework.</p>
+ *
+ * <a name="I18nUtility">
+ * <h3>I18nUtility</h3>
+ * </a>
+ * <p>The {@link com.hp.it.spf.xa.i18n.portal.I18nUtility} class provides methods for:
+ * <ul>
+ *	<li>accessing message resources for your Vignette component (augmented with
+ *	inherent SPF capabilities such as the <code>&lt;contextual_help&gt;...&lt;/contextual_help&gt;</code>
+ *	and <code>&lt;no_localization&gt;...&lt;/no_localization&gt;</code>
+ *	tokens)</li>
+ *	<li>accessing secondary support files (eg static resource files) for your Vignette component</li>
+ *	<li>getting locales configured for your Vignette portal site: all available locales, or just the default locale</li>
+ *	<li>and more.</li>
+ * </ul>
+ * Please see the class documentation for more information.
+ * </p>
+ */
+package com.hp.it.spf.xa.i18n.portal;
