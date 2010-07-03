294e2c98a53d990bfccb021b426c06bb3cd1d20b
Scott Jorgenson <scott.jorgenson@hp.com>
2010-07-03 00:12:51 +0000

    Finished portal utilities package JavaDocs; now just the overview file remains.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk/portal@4228 fbe1f26e-0b55-418f-9102-102d79b04d0f

---
+++ spf-portal-utilities/src/main/java/com/hp/it/spf/xa/misc/portal/package-info.java
@@ -0,0 +1,30 @@
+/**
+ * <p>
+ * This package contains miscellaneous utility classes for Vignette portal
+ * components in Shared Portal Framework.
+ * </p>
+ * 
+ * <a name="Consts"> <h3>Consts</h3> </a>
+ * <p>
+ * The {@link com.hp.it.spf.xa.misc.portal.Consts} class defines many global
+ * attributes potentially useful for SPF-based Vignette portal components.
+ * Generally they are all <code>static</code> and <code>final</code>. For
+ * example, the keynames for the user data properties are defined in this class
+ * - those are the keynames you use with the
+ * {@link com.hp.it.spf.xa.misc.portal.Utils#getUserProperty(PortalContext,String)}
+ * method. Please see the class documentation for more information.
+ * </p>
+ * 
+ * <a name="Utils"> <h3>Utils</h3> </a>
+ * <p>
+ * The {@link com.hp.it.spf.xa.misc.portal.Utils} class defines general-purpose
+ * utility methods potentially useful for SPF-based Vignette components.
+ * Generally they are all <code>static</code> methods. For example, using this
+ * class you can get data about the user (eg, whether or not the user is
+ * logged-in; and properties/attributes of the user) and about the portal
+ * request (eg, the current request URL). Please see the class documentation for
+ * more information.
+ * </p>
+ */
+package com.hp.it.spf.xa.misc.portal;
+
