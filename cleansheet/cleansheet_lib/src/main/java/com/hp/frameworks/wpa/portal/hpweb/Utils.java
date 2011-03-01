095e7ffe6822a8b20e02d6d95cf8f9e8a43cafb6
Zhizhong Zhao <zhizhong.zhao@hp.com>
2011-03-01 21:25:19 +0000

    moved scriplet from jsp to Utils
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk/spf-portal-laf@4598 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- cleansheet/cleansheet_lib/src/main/java/com/hp/frameworks/wpa/portal/hpweb/Utils.java
+++ cleansheet/cleansheet_lib/src/main/java/com/hp/frameworks/wpa/portal/hpweb/Utils.java
@@ -430,7 +430,118 @@ public class Utils {
     	 hpwebModel.setCountryTag(countryTag);
     	 hpwebModel.setLocale(locale);
     	 
-    	 
     	 return hpwebModel;
+     }
+     
+     public static void initHorzNav(PortalContext portalContext, PageContext pageContext) throws Exception {
+    	 Style currentStyle = portalContext.getCurrentStyle();
+    	 String i18nID = currentStyle.getUID();
+    	 
+    	 pageContext.setAttribute("stylePath", portalContext.getPortalHttpRoot() + 
+    	 		currentStyle.getUrlSafeRelativePath());
+
+    	 String whiteCaretImg = 
+    	 	"http://welcome.hp-ww.com/img/hpweb_1-2_tbl_sort_wht.gif";
+    	 String blackCaretImg = 
+    	 	"http://welcome.hp-ww.com/img/hpweb_1-2_tbl_sort_blk.gif";
+    	 String spacerImg =
+    	 	"http://welcome.hp-ww.com/img/s.gif";
+
+    	 if (pageContext.getRequest().getScheme().equals("https")) {
+    	 	whiteCaretImg = 
+    	 		"https://secure.hp-ww.com/img/hpweb_1-2_tbl_sort_wht.gif";
+    	 	blackCaretImg = 
+    	 		"https://secure.hp-ww.com/img/hpweb_1-2_tbl_sort_blk.gif";
+    	 	spacerImg =
+    	 		"https://secure.hp-ww.com/img/s.gif";
+    	 }
+
+    	 pageContext.setAttribute("whiteCaretImg", whiteCaretImg);
+    	 pageContext.setAttribute("blackCaretImg", blackCaretImg);
+    	 pageContext.setAttribute("spacerImg", spacerImg);
+
+    	 pageContext.setAttribute("helpTextDef", Utils.getI18nValue(i18nID, "hpweb.helpText",
+    	 		portalContext));
+    	 pageContext.setAttribute("helpUrlDef",  Utils.getI18nValue(i18nID, "hpweb.helpUrl",
+    	 		portalContext));
+
+    	 HttpServletRequest request = portalContext.getHttpServletRequest();
+    	 HPWebModel hpwebModel = (HPWebModel) request.getAttribute("HPWebModel");
+    	 if (hpwebModel == null) {
+    		 hpwebModel = new HPWebModel(); 
+    		 request.setAttribute("HPWebModel", hpwebModel);
+    	 }
+
+    	 List menuItemList = hpwebModel.getTopMenuItems();
+
+    	 // if the HPWebModel bean has top menu items, use it for hpweb layout
+    	 // top nav, i.e. don't need to generate it from vap configured nav menu.
+
+    	 if (menuItemList == null || menuItemList.size() == 0) {
+    	 	
+    	 	int navSplit = 0;
+    	 	int maxLevel = 3;
+    	 	menuItemList = Utils.getConfiguredMenuItems(portalContext, navSplit, maxLevel);
+    	 }
+
+    	 // We should expect that only one MenuItem node has its highLighted flag 
+    	 // set to true.   Find that selected node and save the tab index it
+    	 // belongs under.   Also if it is a 3rd level node, set the highlight flag
+    	 // of its parent node (a button item) to true.
+
+    	 int selectedIndex = 0;
+    	 boolean done = false;
+    	 for (int tabIndex=0; tabIndex<menuItemList.size(); tabIndex++) {
+    	 	MenuItem tab = (MenuItem) menuItemList.get(tabIndex);
+    	 	if (tab.isVisible() == false) {
+    	 		continue;
+    	 	}
+    	 	if (tab.isHighlighted()) {
+    	 		// found selected node in tab item, don't need to do anymore
+    	 		selectedIndex = tabIndex;
+    	 		break;
+    	 	}
+    	 	int buttonCount = tab.getSubMenuItems().size();
+    	 	if (buttonCount > 0) {
+    	 		for (int buttonIndex=0; buttonIndex<buttonCount; buttonIndex++) {
+    	 			MenuItem button = 
+    	 				(MenuItem) tab.getSubMenuItems().get(buttonIndex);
+    	 			if (button.isVisible() == false) {
+    	 				continue;
+    	 			}
+    	 			if (button.isHighlighted()) {
+    	 				// found selected node in button item, save tab index
+    	 				selectedIndex = tabIndex;
+    	 				done = true;
+    	 				break;
+    	 			}
+    	 			int flyoutItemCount = button.getSubMenuItems().size();
+    	 			if (flyoutItemCount > 0) {
+    	 				for (int flyoutIndex=0; flyoutIndex<flyoutItemCount; flyoutIndex++) {
+    	 					MenuItem flyoutItem = 
+    	 						(MenuItem) button.getSubMenuItems().get(flyoutIndex);
+    	 					if (button.isVisible() == false) {
+    	 						continue;
+    	 					}
+    	 					if (flyoutItem.isHighlighted()) {
+    	 						// found selected node in flyout item, save tab
+    	 						// index and button (parent) item's  highlight flag 
+    	 						// to true.
+    	 						selectedIndex = tabIndex;
+    	 						button.setHighlighted(true);
+    	 						done = true;
+    	 						break;
+    	 					}
+    	 				}
+    	 			}
+    	 		}
+    	 	}
+    	 	if (done)
+    	 		break;
+    	 }
+
+    	 pageContext.setAttribute("menuItemList", menuItemList);
+    	 pageContext.setAttribute("selectedIndex", selectedIndex); 
+    	 
      }
 }
