7ba90a42717247a6bfab76a982538de4b6d3f9b4
Slawomir Zachcial <slawomir.zachcial@hp.com>
2011-08-04 22:04:09 +0000

    Moving setup portal setup code from .post-install.sh to setupPrimaryPortalNode.sh.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/branches/portal-installer@5204 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- portal/spf-portal-installer/src/main/dist/bin/setupPrimaryPortalNode.sh
+++ portal/spf-portal-installer/src/main/dist/bin/setupPrimaryPortalNode.sh
@@ -18,6 +18,9 @@ echo "Setting up Vignette Portal database ${vignette_db_url}"
 # Will use Vignette-provided tools to do that
 pushd ${VIGNETTE_HOME}/bin
 
+
+##### Setup portal database #####
+
 echo "Creating portal tables"
 sh ./runs_with_classpath.sh com.vignette.tableinstaller.tools.TableInstaller \
  "--driver=${vignette_db_driver_class}" \
@@ -35,6 +38,7 @@ if [ ${last_exit_code} -ne 0 ]; then
     popd
     exit ${last_exit_code}
 fi
+
 
 echo "Creating custom SPF columns"
 alter_sql_path="${VIGNETTE_HOME}/config/spf_vap_alter_users.sql"
@@ -63,6 +67,8 @@ if [ ${last_exit_code} -ne 0 ]; then
 fi
 
 
+##### Setup admin account #####
+
 echo "Creating administrator account ${vignette_admin_username}"
 sh ./create_first_admin_account.sh ${vignette_admin_username} ${vignette_admin_password} \
  1>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.out \
@@ -76,35 +82,6 @@ if [ ${last_exit_code} -ne 0 ]; then
     exit ${last_exit_code}
 fi
 
-echo "Bootstraping portal application"
-sh ./war_tool.sh portal.war \
- 1>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.out \
- 2>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.err
-
-last_exit_code=$?
-if [ ${last_exit_code} -ne 0 ]; then
-    echo "Bootstraping portal application failed with code ${last_exit_code}."
-    echo "Aborting."
-    popd
-    exit ${last_exit_code}
-fi
-
-echo "Adding anonymous users"
-user_data_path="${VIGNETTE_HOME}/config/spf_anonymous_users.txt"
-if ${using_cygwin}; then
-    user_data_path="$(cygpath -am ${user_data_path})"
-fi
-sh ./runs_with_classpath.sh com.hp.it.spf.sso.portal.AnonUsersImport  "hpp_realm1" "${user_data_path}" \
- 1>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.out \
- 2>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.err
-
-last_exit_code=$?
-if [ ${last_exit_code} -ne 0 ]; then
-    echo "Adding anonymous users failed with code ${last_exit_code}."
-    echo "Aborting."
-    popd
-    exit ${last_exit_code}
-fi
 
 echo "Updating administrator account ${vignette_admin_username} profile id"
 update_sql_path="${VIGNETTE_HOME}/config/spf_vap_update_admin.sql"
@@ -131,6 +108,152 @@ if [ ${last_exit_code} -ne 0 ]; then
     popd
     exit ${last_exit_code}
 fi
+
+
+##### Bootstrap portal #####
+
+echo "Bootstraping portal application"
+sh ./war_tool.sh portal.war \
+ 1>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.out \
+ 2>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.err
+
+last_exit_code=$?
+if [ ${last_exit_code} -ne 0 ]; then
+    echo "Bootstraping portal application failed with code ${last_exit_code}."
+    echo "Aborting."
+    popd
+    exit ${last_exit_code}
+fi
+
+
+##### Add anonymous users #####
+
+echo "Adding anonymous users"
+user_data_path="${VIGNETTE_HOME}/config/spf_anonymous_users.txt"
+if ${using_cygwin}; then
+    user_data_path="$(cygpath -am ${user_data_path})"
+fi
+sh ./runs_with_classpath.sh com.hp.it.spf.sso.portal.AnonUsersImport  "hpp_realm1" "${user_data_path}" \
+ 1>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.out \
+ 2>>${CASFW_HOME}/var/log/vignette-portal/setupPrimaryPortalNode.err
+
+last_exit_code=$?
+if [ ${last_exit_code} -ne 0 ]; then
+    echo "Adding anonymous users failed with code ${last_exit_code}."
+    echo "Aborting."
+    popd
+    exit ${last_exit_code}
+fi
+
+
+##### Register remote portlet servers #####
+
+remote_portlet_servers_path="${VIGNETTE_HOME}/config/spf_remote_portlet_servers.xml"
+if ${using_cygwin}; then
+    remote_portlet_servers_path="$(cygpath -am ${remote_portlet_servers_path})"
+fi
+
+echo "Registering remote portlet servers defined in ${remote_portlet_servers_path}"
+sh ./runs_with_classpath.sh com.hp.it.spf.misc.portal.RegisterRemotePortletServers \
+ ${remote_portlet_servers_path} \
+1>>${CASFW_HOME}/var/log/vignette-portal/registerRemotePortletServers.out \
+2>>${CASFW_HOME}/var/log/vignette-portal/registerRemotePortletServers.err
+
+last_exit_code=$?
+if [ ${last_exit_code} -ne 0 ]; then
+    echo "Registering remote portlet servers failed with code ${last_exit_code}."
+    echo "Aborting."
+    popd
+    exit ${last_exit_code}
+fi
+
+
+##### Register local portlet applications #####
+
+portlet_applications_path="${VIGNETTE_HOME}/config/spf_local_portlet_applications.xml"
+if ${using_cygwin}; then
+    portlet_applications_path="$(cygpath -am ${portlet_applications_path})"
+fi
+
+echo "Registering portlet applications defined in ${portlet_applications_path}"
+sh ./runs_with_classpath.sh com.hp.it.spf.misc.portal.RegisterPortletApplications \
+ ${portlet_applications_path} \
+1>>${CASFW_HOME}/var/log/vignette-portal/registerPortletApplications.out \
+2>>${CASFW_HOME}/var/log/vignette-portal/registerPortletApplications.err
+
+last_exit_code=$?
+if [ ${last_exit_code} -ne 0 ]; then
+    echo "Registering portlet applications failed with code ${last_exit_code}."
+    echo "Aborting."
+    popd
+    exit ${last_exit_code}
+fi
+
+
+##### Register portal-supported languages/locales #####
+
+supported_languages_path="${CASFW_HOME}/etc/resources_global/site_locale_support.properties"
+if ${using_cygwin}; then
+    supported_languages_path="$(cygpath -am ${supported_languages_path})"
+fi
+
+echo "Adding supported languages defined in ${supported_languages_path}"
+sh ./runs_with_classpath.sh com.hp.it.spf.misc.portal.RegisterSupportedLanguages \
+ ${supported_languages_path} \
+1>>${CASFW_HOME}/var/log/vignette-portal/RegisterSupportLanguages.out \
+2>>${CASFW_HOME}/var/log/vignette-portal/RegisterSupportLanguages.err
+
+last_exit_code=$?
+if [ ${last_exit_code} -ne 0 ]; then
+    echo "Registering support languages failed with code ${last_exit_code}."
+    echo "Aborting."
+    popd
+    exit ${last_exit_code}
+fi
+
+
+##### Import CAR files #####
+
+echo "Y" > ${CASFW_HOME}/var/accept_components_import.txt
+
+function import_car_files {
+    local current_time=
+
+    for car_file in "$@"; do
+        if ${using_cygwin}; then
+            car_file="$(cygpath -am ${car_file})"
+        fi
+        echo "Importing ${car_file}"
+
+        current_time="$(date '+%Y-%m-%d %H:%M:%S')"
+
+        echo "==== ${current_time} - Importing ${car_file} ====" >> ${CASFW_HOME}/var/log/vignette-portal/ImportExportTool.out
+        echo "==== ${current_time} - Importing ${car_file} ====" >> ${CASFW_HOME}/var/log/vignette-portal/ImportExportTool.err
+
+        ./ImportExportTool.sh -import ${car_file} < ${CASFW_HOME}/var/accept_components_import.txt \
+        1>>${CASFW_HOME}/var/log/vignette-portal/ImportExportTool.out \
+        2>>${CASFW_HOME}/var/log/vignette-portal/ImportExportTool.err
+
+        last_exit_code=$?
+        if [ ${last_exit_code} -ne 0 ]; then
+            echo "Importing file ${car_file} failed with code ${last_exit_code}."
+            echo "Aborting."
+            popd
+            rm ${CASFW_HOME}/var/accept_components_import.txt
+            exit ${last_exit_code}
+        fi
+    done
+}
+
+echo "Importing portal sites"
+import_car_files $(ls ${CASFW_HOME}/software/*.car | grep "\-site-")
+
+# We imported those already during bootstrap, but let's reimport them to ensure that if the site CARs
+#  came with older components the newer ones packaged in these CARs will be used.
+echo "Importing remaining component archives"
+import_car_files $(ls ${CASFW_HOME}/software/*.car | grep -v "\-site-")
+
+rm ${CASFW_HOME}/var/accept_components_import.txt
 
 popd
 
