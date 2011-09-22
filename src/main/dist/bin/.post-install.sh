5b92842ee454f420b46feb51537842205dedd88b
Slawomir Zachcial <slawomir.zachcial@hp.com>
2011-09-22 14:40:06 +0000

    Merging portal-installer branch into trunk. There are still some duplicate files introduced with portal-installer that require clean-up.
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk/portal/spf-portal-installer@5268 fbe1f26e-0b55-418f-9102-102d79b04d0f

---
+++ src/main/dist/bin/.post-install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CASFW_HOME="$(cd "$(dirname "$0")/.." && pwd -P)" 
+
+source ${CASFW_HOME}/bin/.casfwrc
+
+# Setup DB for portal
+${CASFW_HOME}/bin/setupPrimaryPortalNode.sh
+
+# Setup DB for persona
+${CASFW_HOME}/bin/setupPersonaDatabase.sh
+
+#Fix permissions for all other files
+echo "Setting other permissions"
+
+# In general we want:
+# - user to read+write+browse (i.e. execute for directories, and if execute for files was already there we are fine),
+# - group to read+browse,
+# - others to do nothing
+# - everybody to have read+browse access to log directory
+chmod -R u+rwX,g=rX,o= ${CASFW_HOME}
+chmod a+rX ${CASFW_HOME}
+chmod -R a+rX ${CASFW_HOME}/var
+chmod -R a+rX ${CASFW_HOME}/var/log
+
+# allow other installers using this one as base to provide their additional steps
+if [ -r ${CASFW_HOME}/bin/.post-install_custom.sh ]; then
+    source ${CASFW_HOME}/bin/.post-install_custom.sh
+fi
+
+# Print message about URL at which Portal runs
+tomcat_portal_http_port="$(get_property_value "${CASFW_HOME}/etc/casfw.properties" "tomcat_portal_connector_http_port")"
+echo
+echo "Starting Tomcat with Vignette Portal at http://$(hostname):${tomcat_portal_http_port}/portal/"
+${CASFW_HOME}/bin/tomcat-portal.sh start
+
+
+#echo
+#echo "Please check ${CASFW_HOME}/README.txt for details of the components included"
+#echo "in this installation."
