5337335f43778615811477e705a8c0fd0ed79f56
Jie Shi <jie.shi@hp.com>
2015-08-31 04:31:47 +0000

    shell script for consumer servers
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk@5760 fbe1f26e-0b55-418f-9102-102d79b04d0f

---
+++ ncs-scripts/g1u1699_austin_hp_com.sh
@@ -0,0 +1,8 @@
+./portcheck.pl -t c0041754.itcs.hp.com -p 8080 2>&1 | tee -a portLogg1u1699austinhpcom.txt
+./portcheck.pl -t c0044895.itcs.hp.com -p 8080 2>&1 | tee -a portLogg1u1699austinhpcom.txt
+./portcheck.pl -t c2t02039.itcs.hp.com -p 8080 2>&1 | tee -a portLogg1u1699austinhpcom.txt
+./portcheck.pl -t c4t02041.itcs.hp.com -p 8080 2>&1 | tee -a portLogg1u1699austinhpcom.txt
+./portcheck.pl -t c4t02042.itcs.hp.com -p 8080 2>&1 | tee -a portLogg1u1699austinhpcom.txt
+
+# link to LDAP
+./portcheck.pl -t ed.hpicorp.net -p 389  2>&1 | tee -a portLogg2u1699austinhpcom.txt
