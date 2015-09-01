716c2057cd5e567c171dc33361294b41b06b7a9f
Ye Liu <ye.liu@hp.com>
2015-09-01 02:24:48 +0000

    Upgraded to PortTool version 1.4
    git-svn-id: file:///opt/mount/casfw/spf-git-migration/spf.svn/source/trunk@5761 fbe1f26e-0b55-418f-9102-102d79b04d0f

--- ncs-scripts/PortQtySTUB.bat
+++ ncs-scripts/PortQtySTUB.bat
@@ -1,3 +1,3 @@
-portqry.exe -n DEST.SERVERNAME.HERE -e DEST_PORT_NUMBER_HERE>>portLog.txt
+portqry.exe -n DEST.SERVERNAME.HERE -p ENTER_TCP_OR_UDP_HERE -e DEST_PORT_NUMBER_HERE>>portLog.txt
 
-REM Example: portqry.exe -n g1u3018c.austin.hpicorp.net -e 22>>portLog.txt
+REM Example: portqry.exe -n g1u3018c.austin.hpicorp.net -p TCP -e 22>>portLog.txt
